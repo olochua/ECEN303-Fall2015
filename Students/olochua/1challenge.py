c0831daee2a158786796ebfc65f59a27331a9933
olochua <olochua@tamu.edu>
2015-09-06 16:30:32 -0500

    Update 1challenge.py

--- Students/olochua/1challenge.py
+++ Students/olochua/1challenge.py
@@ -1,10 +1,10 @@
-__author__ = ""
-__NetID__ = ""
-__GitHubID__ = ""
+__author__ = "Jui Yen Chua"
+__NetID__ = "olochua"
+__GitHubID__ = "olochua"
 __challenge__ = "1"
 __version__ = "0.0"
 __grader__ = ""
-__SelfGrade__ = ""
+__SelfGrade__ = "5"
 __PeerGrade__ = ""
 
 """
@@ -17,58 +17,82 @@ import random
 import math
 import matplotlib.pyplot as plt
 
-
-ParameterP = 0.3
+#defining the variables
+ParameterP = 0.7
 NumberFlips = 8
 NumberTrials = 100000
-Trials = []
 
+#this stores the sum of outcome each coin flip test
+temp_val = 0
 
+#this stores the total of individual probability
+total = 0
+
+#function returns a 1 using the probability p, 
+#and returns a 0 with the probability 1-p
 def biasedcoinflip(p=0.5):
-    # EDIT
-    # Create method for biased coin flip
-    #
+    if random.random()<p:
+        return 1
+    else:
+        return 0
 
+#compute NumberTrials of tests and append the result to Trials.
+Trials = []
 
 for TrialIndex1 in range(0, NumberTrials):
     Trials.append(biasedcoinflip(ParameterP))
 
+#The average (which should be very close to the value of ParameterP) of Trials
 TrialAverage = sum(Trials) / (1.0 * len(Trials))
 print 'The average number of ones is {0:.4f}.'.format(TrialAverage)
 
+#compute NumberTrials of NumberFlips-times of coin flip and append the result to SumTrials
 SumTrials = []
 
-for TrialIndex2 in range(0, NumberTrials):
-    # EDIT
-    # Add NumberFlips coin flips for each SumTrials outcome
-    #
+for attempt in range(0,NumberTrials):
+    for TrialIndex2 in range(0, NumberFlips):
+        #sums up the coin flips in a test
+        temp_val = temp_val + biasedcoinflip(ParameterP)  
+    
+    SumTrials.append(temp_val)
+    #after appending the result, reset the variable
+    temp_val = 0
 
+#compute the distribution of the outcome
 Distribution = []
 for OutcomeIndex1 in range(0, NumberFlips + 1):
     Distribution.append(SumTrials.count(OutcomeIndex1) / (1.0 * NumberTrials))
 
 print repr(Distribution)
-# EDIT
-# Print the sum of the elements in Distribution
-#
 
+#sums up individual probability (which should return a 1 since the probabilities are not overlapping)
+for value in Distribution:
+    total = total + value
+
+print "The sum of elements in Distribution is: " + str(total)
+
+#setting up the values to plot the graph
 OutcomeIndex2 = range(0, NumberFlips + 1)
 num_bins = len(OutcomeIndex2)
 bar_width = 0.8
 XticksIndex = [(outcome + (0.5 * bar_width)) for outcome in OutcomeIndex2]
 opacity = 0.4
 
+#plotting the graph with necessary labels
 plt.bar(OutcomeIndex2, Distribution, bar_width, alpha=opacity, color='b')
 plt.xlabel("Value")
 plt.ylabel("Probability")
 plt.xticks(XticksIndex, OutcomeIndex2)
 plt.show()
 
+
 """
 Describe what happens to the figure as you vary ParameterP from zero to one.
-
+As ParameterP increases from 0 to 1, the center of the series of bar graph - where the peak is located - shifts from left to right in the figure.
+As for the remaining bar graphs on both sides of the peak, they gradually decrease towards the edges. In other words, it appears to be a shift in the 
+bell graph.
 
 What is the most likely outcome for ParameterP = 0.7 and NumberFlips = 8?
-
+The most likely outcome happens in value = 6, with a probability of 0.3.
 
 """
