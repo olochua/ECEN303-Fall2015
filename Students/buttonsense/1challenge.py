fb94631a3b1c35eb10a0712b3b7d565b519b0405
Seungwon Yoon <ysw0927@tamu.edu>
2015-09-06 02:43:22 -0500

    Completed challenge 1 and task 3.

--- Students/buttonsense/1challenge.py
+++ Students/buttonsense/1challenge.py
@@ -1,8 +1,8 @@
-__author__ = ""
-__NetID__ = ""
-__GitHubID__ = ""
+__author__ = "Seungwon Yoon"
+__NetID__ = "ysw0927"
+__GitHubID__ = "buttonsense"
 __challenge__ = "1"
-__version__ = "0.0"
+__version__ = "3.4"
 __grader__ = ""
 __SelfGrade__ = ""
 __PeerGrade__ = ""
@@ -23,34 +23,33 @@ NumberFlips = 8
 NumberTrials = 100000
 Trials = []
 
-
+#biasedcoinflip that returns 1 or 0
 def biasedcoinflip(p=0.5):
-    # EDIT
-    # Create method for biased coin flip
-    #
+    if random.random() < p:
+        return 1
+    else:
+        return 0
 
 
 for TrialIndex1 in range(0, NumberTrials):
     Trials.append(biasedcoinflip(ParameterP))
 
 TrialAverage = sum(Trials) / (1.0 * len(Trials))
-print 'The average number of ones is {0:.4f}.'.format(TrialAverage)
+print('The average number of ones is {0:.4f}.'.format(TrialAverage))
 
 SumTrials = []
 
-for TrialIndex2 in range(0, NumberTrials):
-    # EDIT
-    # Add NumberFlips coin flips for each SumTrials outcome
-    #
+SumTrials = (list(sum(Trials[x:x+NumberFlips]) for x in range(0,len(Trials), NumberFlips)))
+#sums NumberFlips outcomes from list Trials and appends the result to list Sumtrials
+#I had no idea how to do this using your for loops
 
 Distribution = []
 for OutcomeIndex1 in range(0, NumberFlips + 1):
     Distribution.append(SumTrials.count(OutcomeIndex1) / (1.0 * NumberTrials))
 
-print repr(Distribution)
-# EDIT
-# Print the sum of the elements in Distribution
-#
+
+print(repr(Distribution))
+print(sum(Distribution)) #print the sum of elements in Distribution
 
 OutcomeIndex2 = range(0, NumberFlips + 1)
 num_bins = len(OutcomeIndex2)
@@ -66,9 +65,10 @@ plt.show()
 
 """
 Describe what happens to the figure as you vary ParameterP from zero to one.
-
+Probability of getting 1(Head) gets bigger as we change ParameterP from zero to one.
 
 What is the most likely outcome for ParameterP = 0.7 and NumberFlips = 8?
+Average number of ones will be close to 0.7 and distribution will show higher values.
 
 
 """
