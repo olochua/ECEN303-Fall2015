dbadd06b27cd9c77e0895d7f63580fd34dcafa75
thenderson37 <tyler.henderson07@tamu.edu>
2015-09-06 13:19:08 -0500

    Update 3task.py

--- Students/thenderson37/3task.py
+++ Students/thenderson37/3task.py
@@ -9,9 +9,9 @@ NumberTrials = 1000
 
 TrialSequence = []
 for TrialIndex in range(0, NumberTrials):
-    #TrialSequence.append(random.randrange(Cardinality))
+    #TrialSequence.append(random.randrange(Cardinality)) this line was no longer needed
     
-    if random.random() <= .75:
+    if random.random() <= .75: #probability of one side of the coin is now .75
         TrialSequence.append(1)
     
     else:
@@ -20,4 +20,4 @@ for TrialIndex in range(0, NumberTrials):
 EmpiricalDistribution = []
 for OutcomeIndex in range(0, Cardinality):
     EmpiricalDistribution.append(TrialSequence.count(OutcomeIndex) / float(NumberTrials))
-print EmpiricalDistribution
+print(EmpiricalDistribution) # added () 
