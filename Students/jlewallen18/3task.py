29da3c4bdd2120049480d80fd731de4dd377f9fb
unknown <jlewallen18@gmail.com>
2015-09-06 16:56:04 -0500

    update task 3

--- Students/jlewallen18/3task.py
+++ Students/jlewallen18/3task.py
@@ -1,22 +1,26 @@
-__author__ = ""
-__NetID__ = ""
-__GitHubID__ = ""
+__author__ = "Jordan Lewallen"
+__NetID__ = "jlewallen18"
+__GitHubID__ = "jlewallen18"
 
 import random
 
 Cardinality = 2
 NumberTrials = 1000
 
+#number of flips
+
+flips = 0
+
 TrialSequence = []
 for TrialIndex in range(0, NumberTrials):
     TrialSequence.append(random.randrange(Cardinality))
-    #
-    # EDIT
-    # Modify code to produce biased binary coin flip that returns one with probability 0.75
-    # and zero otherwise
-    #
+    if ((TrialSequence.count(1)/float(NumberTrials)) < 0.75) == True:
+        TrialSequence[flips] = 1
+    else:
+        TrialSequence[flips] = 0
+    flips = flips + 1
 
 EmpiricalDistribution = []
 for OutcomeIndex in range(0, Cardinality):
     EmpiricalDistribution.append(TrialSequence.count(OutcomeIndex) / float(NumberTrials))
-print EmpiricalDistribution
+print (EmpiricalDistribution)
