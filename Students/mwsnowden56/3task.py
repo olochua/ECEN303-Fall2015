184f191299685e1ea5e25f0a242ddb87ac43a13a
Michael Snowden <mwsnowden56@tamu.edu>
2015-09-07 12:04:28 -0500

    Update 3task.py

--- Students/mwsnowden56/3task.py
+++ Students/mwsnowden56/3task.py
@@ -1,6 +1,6 @@
-__author__ = ""
-__NetID__ = ""
-__GitHubID__ = ""
+__author__ = "Michael Snowden"
+__NetID__ = "mwsnowden56"
+__GitHubID__ = "mwsnowden56"
 
 import random
 
@@ -15,7 +15,11 @@ for TrialIndex in range(0, NumberTrials):
     # Modify code to produce biased binary coin flip that returns one with probability 0.75
     # and zero otherwise
     #
-
+    if random.random() < 0.75:
+        return 1
+    else:
+        return 0
+    
 EmpiricalDistribution = []
 for OutcomeIndex in range(0, Cardinality):
     EmpiricalDistribution.append(TrialSequence.count(OutcomeIndex) / float(NumberTrials))
