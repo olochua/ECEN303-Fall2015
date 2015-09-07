f624889e644cc4541fd5e6a1b3e4f85a6e1a5b24
Shannon Morrissey <shannon.morrissey@email.tamu.edu>
2015-09-06 19:42:12 -0500

    Update 3task.py

--- Students/shmorrissey/3task.py
+++ Students/shmorrissey/3task.py
@@ -1,6 +1,6 @@
-__author__ = ""
-__NetID__ = ""
-__GitHubID__ = ""
+__author__ = "Shannon Morrissey"
+__NetID__ = "shannon.morrissey"
+__GitHubID__ = "shmorrissey"
 
 import random
 
@@ -9,13 +9,10 @@ NumberTrials = 1000
 
 TrialSequence = []
 for TrialIndex in range(0, NumberTrials):
-    TrialSequence.append(random.randrange(Cardinality))
-    #
-    # EDIT
-    # Modify code to produce biased binary coin flip that returns one with probability 0.75
-    # and zero otherwise
-    #
-
+    if random.random() <= 0.75:
+        TrialSequence.append(1)
+    else:
+        TrialSequence.append(0)
 EmpiricalDistribution = []
 for OutcomeIndex in range(0, Cardinality):
     EmpiricalDistribution.append(TrialSequence.count(OutcomeIndex) / float(NumberTrials))
