6eabef5b30da45ec8ff23209215db0b75143cb24
ajdouglass <ajdouglass17@gmail.com>
2015-09-07 00:16:30 -0500

    Update 3task.py

--- Students/ajdouglass/3task.py
+++ Students/ajdouglass/3task.py
@@ -1,22 +1,18 @@
-__author__ = ""
-__NetID__ = ""
-__GitHubID__ = ""
+__author__ = 'Andrew Douglass'
+__NetID__ = "adoulgas"
+__GitHubID__ = "ajdouglass"
 
 import random
 
 Cardinality = 2
 NumberTrials = 1000
 
+CoinFlipBiased = [0, 1, 1, 1]  # 75% biased towards 1
 TrialSequence = []
 for TrialIndex in range(0, NumberTrials):
-    TrialSequence.append(random.randrange(Cardinality))
-    #
-    # EDIT
-    # Modify code to produce biased binary coin flip that returns one with probability 0.75
-    # and zero otherwise
-    #
+    TrialSequence.append(CoinFlipBiased[random.randrange(4)])   # choose random index from CoinFlipBiased list
 
 EmpiricalDistribution = []
 for OutcomeIndex in range(0, Cardinality):
     EmpiricalDistribution.append(TrialSequence.count(OutcomeIndex) / float(NumberTrials))
-print EmpiricalDistribution
+print(EmpiricalDistribution)
