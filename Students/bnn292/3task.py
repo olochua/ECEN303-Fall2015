3ae07963f96fa993fc3685bf1d9569ecd6e2e7f8
Bijan Nekovei <bnn292@tamu.edu>
2015-09-08 08:21:02 -0500

    Update 3task.py

--- Students/bnn292/3task.py
+++ Students/bnn292/3task.py
@@ -9,14 +9,13 @@ NumberTrials = 1000
 
 TrialSequence = []
 for TrialIndex in range(0, NumberTrials):
-    TrialSequence.append(random.randrange(Cardinality))
-    #
-    # EDIT
-    # Modify code to produce biased binary coin flip that returns one with probability 0.75
-    # and zero otherwise
-    #
+    #TrialSequence.append(random.randrange(Cardinality)) #Uneccesary line
+    if random.random() < 0.75: #Change bias to 75% of winning
+        TrialSequence.append(1)   # Win state (75%)
+    else:
+        TrialSequence.append(0)   # Failure state (25%)
 
 EmpiricalDistribution = []
 for OutcomeIndex in range(0, Cardinality):
     EmpiricalDistribution.append(TrialSequence.count(OutcomeIndex) / float(NumberTrials))
-print EmpiricalDistribution
+print(EmpiricalDistribution) #Fixed
