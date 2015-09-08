bcde5c2a30aa8418437c09ee1a4573407006ce69
lgnbarnard <lgn_barnard@tamu.edu>
2015-09-08 02:05:57 -0500

    Update 3task.py

--- Students/lgnbarnard/3task.py
+++ Students/lgnbarnard/3task.py
@@ -1,22 +1,23 @@
-__author__ = ""
-__NetID__ = ""
-__GitHubID__ = ""
+__author__ = "Logan Barnard"
+__NetID__ = "lgn_barnard"
+__GitHubID__ = "lgnbarnard"
 
 import random
+
+print("This program simulates flipping a biased coin 1000 times")
 
 Cardinality = 2
 NumberTrials = 1000
 
 TrialSequence = []
+#used random.random to return floating point rather than random.randrange(Cardinality)
 for TrialIndex in range(0, NumberTrials):
-    TrialSequence.append(random.randrange(Cardinality))
-    #
-    # EDIT
-    # Modify code to produce biased binary coin flip that returns one with probability 0.75
-    # and zero otherwise
-    #
+    if random.random()<.75:
+        TrialSequence.append(1)
+    else:
+        TrialSequence.append(0)
 
 EmpiricalDistribution = []
 for OutcomeIndex in range(0, Cardinality):
     EmpiricalDistribution.append(TrialSequence.count(OutcomeIndex) / float(NumberTrials))
-print EmpiricalDistribution
+print("[Heads, Tails]= ", EmpiricalDistribution)
