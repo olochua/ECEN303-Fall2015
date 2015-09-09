3e9f8d86c546dff9fd88f6b2346ff43d49264a7c
Gissel Gardea <gardegi059@tamu.edu>
2015-09-09 11:23:00 -0500

    Update 3task.py

--- Students/ggardea66/3task.py
+++ Students/ggardea66/3task.py
@@ -6,16 +6,16 @@ import random
 
 Cardinality = 2
 NumberTrials = 1000
-Coins= 2
+Flip= 2
 
 TrialSequence = []
-for TrialIndex in range(0, NumberTrials):
-    TrialSequence.append(random.randrange(Cardinality))
-    if random.random() < .75:
-        Coins = 1
+for TrialIndex in range(0, NumberTrials): #1000 trials are done
+    TrialSequence.append(random.randrange(Cardinality)) 
+    if random.random() < .75: #probability of 75 percent
+        Flip = 1 #represents one side of the coin
     else:
-        Coins = 0
-        TrialSequence.append(Coins)
+        Flip = 0 #represents the other side of the coin
+        TrialSequence.append(Flip) #
 
 EmpiricalDistribution = []
 for OutcomeIndex in range(0, Cardinality):
