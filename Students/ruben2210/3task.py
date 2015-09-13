6f3af80ef8062d530dd981f102813ee0ce31ecf0
ruben2210 <ruben2210@tamu.edu>
2015-09-13 08:23:37 -0500

    Create 3task.py

---
+++ Students/ruben2210/3task.py
@@ -0,0 +1,20 @@
+__author__ = "Ruben Rodriguez"
+__NetID__ = "ruben2210"
+__GitHubID__ = "ruben2210"
+
+import random
+
+Cardinality = 2
+NumberTrials = 1000
+
+TrialSequence = []
+for TrialIndex in range(0, NumberTrials):
+    TrialSequence.append(random.randrange(Cardinality))
+    #
+    # EDIT
+    #
+
+EmpiricalDistribution = []
+for OutcomeIndex in range(0, Cardinality):
+    EmpiricalDistribution.append(TrialSequence.count(OutcomeIndex) / float(NumberTrials))
+print EmpiricalDistribution
