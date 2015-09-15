7224f884c1270aaa089641d6dd2030c3a6995ac0
westort <wes4477@yahoo.com>
2015-09-15 01:08:56 -0500

    update challenge1.m

--- Students/westort/1challenge.py
+++ Students/westort/1challenge.py
@@ -1,10 +1,10 @@
-__author__ = ""
-__NetID__ = ""
-__GitHubID__ = ""
+__author__ = "Weston Torti"
+__NetID__ = "tort115"
+__GitHubID__ = "westort"
 __challenge__ = "1"
 __version__ = "0.0"
 __grader__ = ""
-__SelfGrade__ = ""
+__SelfGrade__ = "4"
 __PeerGrade__ = ""
 
 """
@@ -17,17 +17,18 @@ import random
 import math
 import matplotlib.pyplot as plt
 
-
-ParameterP = 0.3
+ParameterP = 0.7
 NumberFlips = 8
 NumberTrials = 100000
 Trials = []
+TrialSum = 0
 
 
 def biasedcoinflip(p=0.5):
-    # EDIT
-    # Create method for biased coin flip
-    #
+    if random.random() <= p:  # gives probability with value of parameter p
+        return 1
+    else:  # since random goes from 0 to 1 else will give 0's with value 1-p
+        return 0
 
 
 for TrialIndex1 in range(0, NumberTrials):
@@ -35,22 +36,22 @@ for TrialIndex1 in range(0, NumberTrials):
 
 TrialAverage = sum(Trials) / (1.0 * len(Trials))
 print 'The average number of ones is {0:.4f}.'.format(TrialAverage)
-
 SumTrials = []
-
-for TrialIndex2 in range(0, NumberTrials):
-    # EDIT
-    # Add NumberFlips coin flips for each SumTrials outcome
-    #
+for TrialIndex2 in range(0, NumberTrials):#going through each trial
+    CoinSum = 0
+    for TrialIndex3 in range(0, NumberFlips):#going through each flip per trial
+        CoinSum += biasedcoinflip(ParameterP)
+    SumTrials.append(CoinSum) #appending the most recent trials sum to the trial sum
 
 Distribution = []
 for OutcomeIndex1 in range(0, NumberFlips + 1):
     Distribution.append(SumTrials.count(OutcomeIndex1) / (1.0 * NumberTrials))
 
+
+DistributionSum= 0
+DistributionSum = sum(Distribution)
 print repr(Distribution)
-# EDIT
-# Print the sum of the elements in Distribution
-#
+print repr(DistributionSum)
 
 OutcomeIndex2 = range(0, NumberFlips + 1)
 num_bins = len(OutcomeIndex2)
@@ -66,9 +67,10 @@ plt.show()
 
 """
 Describe what happens to the figure as you vary ParameterP from zero to one.
-
+As ParameterP is varied from zero to one, The figure is shifted from left to right along the x axis, this is because
+the probability for getting a one is becoming more and more likely therefore increasing the count of 1's in a trial
 
 What is the most likely outcome for ParameterP = 0.7 and NumberFlips = 8?
 
-
+The most likely outcome with the given inputs is 6
 """
