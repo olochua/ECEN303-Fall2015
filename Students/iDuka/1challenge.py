ed4a322ba597744d0580f1a208a55f17e7fc8841
Isach Duka <iDuka95@live.com>
2015-09-15 00:48:27 -0500

    Update 1challenge.py

--- Students/iDuka/1challenge.py
+++ Students/iDuka/1challenge.py
@@ -1,8 +1,8 @@
-__author__ = ""
-__NetID__ = ""
-__GitHubID__ = ""
+__author__ = "Mirel Duka"
+__NetID__ = "iDuka"
+__GitHubID__ = "iDuka"
 __challenge__ = "1"
-__version__ = "0.0"
+__version__ = "0.9"
 __grader__ = ""
 __SelfGrade__ = ""
 __PeerGrade__ = ""
@@ -25,32 +25,37 @@ Trials = []
 
 
 def biasedcoinflip(p=0.5):
-    # EDIT
-    # Create method for biased coin flip
-    #
+    #Edit
+    if random.random() < p:
+        return 1
+    else:
+        return 0
 
 
 for TrialIndex1 in range(0, NumberTrials):
     Trials.append(biasedcoinflip(ParameterP))
 
 TrialAverage = sum(Trials) / (1.0 * len(Trials))
-print 'The average number of ones is {0:.4f}.'.format(TrialAverage)
+print ('The average number of ones is {0:.4f}.'.format(TrialAverage))
 
 SumTrials = []
 
 for TrialIndex2 in range(0, NumberTrials):
-    # EDIT
-    # Add NumberFlips coin flips for each SumTrials outcome
-    #
+    #Edited
+    sum_i = 0
+    for ind in range(0,NumberFlips):
+        sum_i += biasedcoinflip(ParameterP)
+    SumTrials.append(sum_i)
 
 Distribution = []
 for OutcomeIndex1 in range(0, NumberFlips + 1):
     Distribution.append(SumTrials.count(OutcomeIndex1) / (1.0 * NumberTrials))
 
-print repr(Distribution)
+print(repr(Distribution))
 # EDIT
 # Print the sum of the elements in Distribution
-#
+dist_sum = sum(Distribution)
+print("The Sum of the distributions is %d" % dist_sum)
 
 OutcomeIndex2 = range(0, NumberFlips + 1)
 num_bins = len(OutcomeIndex2)
@@ -66,9 +71,5 @@ plt.show()
 
 """
 Describe what happens to the figure as you vary ParameterP from zero to one.
-
-
 What is the most likely outcome for ParameterP = 0.7 and NumberFlips = 8?
-
-
 """
