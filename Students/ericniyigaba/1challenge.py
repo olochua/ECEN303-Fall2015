fb2f70e06d20cd63a6f46959efddd75a9f2f8839
ericniyigaba <ericniyigaba@tamu.edu>
2015-09-15 23:21:06 -0500

    Update 1challenge.py

--- Students/ericniyigaba/1challenge.py
+++ Students/ericniyigaba/1challenge.py
@@ -7,12 +7,12 @@ __grader__ = ""
 __SelfGrade__ = ""
 __PeerGrade__ = ""
 
+
 """
 Random Signals and Systems
 Course: ECEN 303-502
 Maximum Grade: 5pt
 """
-
 import random
 import math
 import matplotlib.pyplot as plt
@@ -25,9 +25,10 @@ Trials = []
 
 
 def biasedcoinflip(p=0.5):
-    # EDIT
-    # Create method for biased coin flip
-    #
+      if random.random() <= p:
+        return 1
+      else:
+        return 0
 
 
 for TrialIndex1 in range(0, NumberTrials):
@@ -39,18 +40,22 @@ print 'The average number of ones is {0:.4f}.'.format(TrialAverage)
 SumTrials = []
 
 for TrialIndex2 in range(0, NumberTrials):
-    # EDIT
-    # Add NumberFlips coin flips for each SumTrials outcome
-    #
+    NumberHeads = 0
+
+    for TrialIndex3 in range(0, NumberFlips):
+        NumberHeads += biasedcoinflip(ParameterP)
+    
+    SumTrials.append(NumberHeads)
 
 Distribution = []
-for OutcomeIndex1 in range(0, NumberFlips + 1):
+for OutcomeIndex1 in range(0, NumberFlips+1):
     Distribution.append(SumTrials.count(OutcomeIndex1) / (1.0 * NumberTrials))
 
 print repr(Distribution)
-# EDIT
-# Print the sum of the elements in Distribution
-#
+Sum = 0
+for OutcomeIndex3 in range(0, NumberFlips + 1):
+    Sum += Distribution[OutcomeIndex3]
+print(Sum)
 
 OutcomeIndex2 = range(0, NumberFlips + 1)
 num_bins = len(OutcomeIndex2)
@@ -66,9 +71,10 @@ plt.show()
 
 """
 Describe what happens to the figure as you vary ParameterP from zero to one.
-
-
+ As ParameterP approaches 0,the probability distribution tend to approach 0 as well.
+ the same thing happens as the probability approaches 1, the distribution also approcheases 1 too.
+ Also when ParameterP = 0, the probability distribution is located at 0 but for Parameter= 1, 
+ the probability is on the top of 1 at ParameterP = 1,
+ 
 What is the most likely outcome for ParameterP = 0.7 and NumberFlips = 8?
-
-
-"""
+the most likely is 6 heads out of 8 flips.  as it can be calculated: 0.7*8 = 5.6 which is almost 6
