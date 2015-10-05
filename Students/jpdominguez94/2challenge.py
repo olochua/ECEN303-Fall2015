fba3978a4adcca0780c12e36a3230b38225eeb97
unknown <jpdominguez94@gmail.com>
2015-10-05 17:14:13 -0500

    Challenge 2

--- Students/jpdominguez94/2challenge.py
+++ Students/jpdominguez94/2challenge.py
@@ -7,7 +7,6 @@ __grader__ = ""
 __SelfGrade__ = ""
 __PeerGrade__ = ""
 
-#***Written Using Python 3***
 """
 Random Signals and Systems
 Course: ECEN 303-502
@@ -23,89 +22,69 @@ ParameterP = 1.0/3.0    # Parameter of digital coin
 ParameterA = 1.0/3.0    # Parameter of digital coin A
 ParameterB = 1.0/2.0    # Parameter of digital coin B
 NumberTrials = 100000
-testP = 4 #Test Parameter for gemetric flips
 
 
 def biasedcoinflip(p=0.5):
-#    """
-#    This method returns a one with probability p and it returns a zero with
-#    probability (1 - p). The default parameter is p=0.5; this can be changed
-#    by passing an argument to the method.
-#    """
+    """
+    This method returns a one with probability p and it returns a zero with
+    probability (1 - p). The default parameter is p=0.5; this can be changed
+    by passing an argument to the method.
+    """
     return math.floor(random.random() + p)
 
 
 def geometricflip(p=0.5):
-#    """
-#    This method returns a natural number that denotes the number of digital
-#    coin flips needed to obtain a one. It relies on method biasedcoinflip().
-#    """
-	numberflips = 1
-	while biasedcoinflip(p) != 1:
-		numberflips += 1
-	return numberflips
+    """
+    This method returns a natural number that denotes the number of digital
+    coin flips needed to obtain a one. It relies on method biasedcoinflip().
+    """
+    numberflips = 1
+    while biasedcoinflip(p) != 1:
+        numberflips += 1
+    return numberflips
 
 
-print ("\n\nPart 1\n")
+print "Part 1\n"
 
 Trials = []
-trialP_hits=0
-EvenTrials = 0
 for TrialIndex1 in range(0, NumberTrials):
-	
-	t=geometricflip(ParameterP)
+    Trials.append(geometricflip(ParameterP))
+#
+# EDIT
+#
 
-	if t==testP:
-		trialP_hits+=1
+print "The empirical probability that the  number of flips is 4 is " \
+    # EDIT: + repr(Solution1)) \
+    + "."
 
-	if t%2==0:
-		EvenTrials+=1
+EvenTrials = 0
+for TrialIndex2 in range(0, NumberTrials):
+    #
+    # EDIT
+    #
 
-	Trials.append(t)
-
-
-Solution1=trialP_hits/NumberTrials
-Solution2=trialP_hits/EvenTrials
-
-print("The empirical probability that the  number of flips is 4 is "  + repr(Solution1) + ".\n\n")
-
-print ("The empirical probability that the number of \nflips is 4 conditional on number of flips being even is " + repr(Solution2)  + ".")
-
-
-print ("\n\n\nPart 2\n")
+print "The empirical probability that the number of flips is 4 conditional on number of flips being even is " \
+    # EDIT: + repr(Solution2)) \
+    + "."
 
 
-
+print "\nPart 2\n"
 
 Trials2 = []
 FinalA = 0
 FinalB = 0
-twoFlips=0
 for TrialIndex2 in range(0, NumberTrials):
-	trialA=0
-	trialB=0
-	while trialA==trialB:	
-		trialA+=geometricflip(ParameterA)
-		trialB+=geometricflip(ParameterB)
-	if trialA<trialB:
-		Trials2.append(trialA)
-		FinalA+=1
-	else:
-		Trials2.append(trialB)
-		FinalB+=1
-	if trialB==2 or trialA==2:
-		twoFlips+=1
-	
-		
-		
-Solution3=twoFlips/NumberTrials
-Solution4=FinalA/NumberTrials
-Solution5=FinalB/NumberTrials
+    #
+    # EDIT
+    #
 
-   
-print ("The empirical probability that the number of flips is 2 is \n" + repr(Solution3)  + ".\n\n")
-
-print ("The empirical probability that coin A is showing 1 when the stopping condition is \nmet is " + repr(Solution4) + ".\n\n")
-
-print ("The empirical probability that coin B is showing 1 when the stopping condition is \nmet is " + repr(Solution5) + ".\n\n")
+print "The empirical probability that the number of flips is 2 is " \
+    # EDIT: + repr(Solution3)) \
+    + "."
+print "The empirical probability that coin A is showing 1 when the stopping condition is met is " \
+    # EDIT: + repr(Solution4)) \
+    + "."
+print "The empirical probability that coin B is showing 1 when the stopping condition is met is " \
+    # EDIT: + repr(Solution5)) \
+    + "."
 
