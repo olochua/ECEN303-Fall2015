16abd392219db18fa70e1148e2d263c5ab07a1f8
Morgan Frakes <mgfrakes13@tamu.edu>
2015-10-05 17:55:43 -0500

    Challenge 2
    Completion of Challenge 2

--- Students/mgfrakes/2challenge.py
+++ Students/mgfrakes/2challenge.py
@@ -1,10 +1,10 @@
-__author__ = ""
-__NetID__ = ""
-__GitHubID__ = ""
+__author__ = "Morgan Frakes"
+__NetID__ = "mgfrakes13"
+__GitHubID__ = "mgfrakes"
 __challenge__ = "2"
-__version__ = "0.0"
+__version__ = "1.0"
 __grader__ = ""
-__SelfGrade__ = ""
+__SelfGrade__ = "5.0"
 __PeerGrade__ = ""
 
 """
@@ -47,44 +47,52 @@ def geometricflip(p=0.5):
 print "Part 1\n"
 
 Trials = []
+Pr4 = 0
 for TrialIndex1 in range(0, NumberTrials):
     Trials.append(geometricflip(ParameterP))
-#
-# EDIT
-#
+    if Trials[TrialIndex1] == 4:
+        Pr4 += 1.0
 
-print "The empirical probability that the  number of flips is 4 is " \
-    # EDIT: + repr(Solution1)) \
-    + "."
+Solution1 = Pr4 / NumberTrials
+
+print "The empirical probability that the  number of flips is 4 is " + repr(Solution1) + "."
 
 EvenTrials = 0
 for TrialIndex2 in range(0, NumberTrials):
-    #
-    # EDIT
-    #
+    if Trials[TrialIndex2] % 2 == 0:
+        EvenTrials += 1.0
+PrE = EvenTrials / NumberTrials
+Solution2 = Solution1 / PrE
 
 print "The empirical probability that the number of flips is 4 conditional on number of flips being even is " \
-    # EDIT: + repr(Solution2)) \
-    + "."
-
+      + repr(Solution2) + "."
 
 print "\nPart 2\n"
 
 Trials2 = []
-FinalA = 0
-FinalB = 0
+Pr2 = PrA = PrB = 0
 for TrialIndex2 in range(0, NumberTrials):
-    #
-    # EDIT
-    #
+    FinalA = FinalB = Flips = 0
+    while FinalA == FinalB:
+        FinalA = biasedcoinflip(ParameterA)
+        FinalB = biasedcoinflip(ParameterB)
+        Flips += 1
+    Trials2.append(Flips)
+    if Trials2[TrialIndex2] == 2:
+        Pr2 += 1.0
+    if FinalA == 1:
+        PrA += 1.0
+    if FinalB == 1:
+        PrB += 1.0
 
-print "The empirical probability that the number of flips is 2 is " \
-    # EDIT: + repr(Solution3)) \
-    + "."
+Solution3 = Pr2 / NumberTrials
+Solution4 = PrA / NumberTrials
+Solution5 = PrB / NumberTrials
+
+print "The empirical probability that the number of flips is 2 is " + repr(Solution3) + "."
+
 print "The empirical probability that coin A is showing 1 when the stopping condition is met is " \
-    # EDIT: + repr(Solution4)) \
-    + "."
-print "The empirical probability that coin B is showing 1 when the stopping condition is met is " \
-    # EDIT: + repr(Solution5)) \
-    + "."
+      + repr(Solution4) + "."
 
+print "The empirical probability that coin B is showing 1 when the stopping condition is met is " \
+      + repr(Solution5) + "."
