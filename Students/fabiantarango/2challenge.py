354acc04a1eec8efe025ca349c2e9a4fcdd53585
fabiantarango <fabian.tarango@tamu.edu>
2015-10-06 01:19:11 -0500

    Update 2challenge.py

--- Students/fabiantarango/2challenge.py
+++ Students/fabiantarango/2challenge.py
@@ -1,10 +1,10 @@
-__author__ = ""
-__NetID__ = ""
-__GitHubID__ = ""
+__author__ = "Fabian Tarango"
+__NetID__ = "fabian.tarango"
+__GitHubID__ = "fabiantarango"
 __challenge__ = "2"
-__version__ = "0.0"
+__version__ = "2.7"
 __grader__ = ""
-__SelfGrade__ = ""
+__SelfGrade__ = "4"
 __PeerGrade__ = ""
 
 """
@@ -49,42 +49,53 @@ print "Part 1\n"
 Trials = []
 for TrialIndex1 in range(0, NumberTrials):
     Trials.append(geometricflip(ParameterP))
-#
-# EDIT
-#
+PrFour = round(Trials.count(4)/float(len(Trials)),5)  #The empirical probability that the  number of flips is 4
 
-print "The empirical probability that the  number of flips is 4 is " \
-    # EDIT: + repr(Solution1)) \
-    + "."
+print "The empirical probability that the  number of flips is 4 is " + repr(PrFour) + "."
 
 EvenTrials = 0
 for TrialIndex2 in range(0, NumberTrials):
-    #
-    # EDIT
-    #
+    if Trials[TrialIndex2]%2 == 0:  #Trials is a multiple of 2
+        EvenTrials +=1
+PrEven = round(Trials.count(4)/float(EvenTrials),5)
 
-print "The empirical probability that the number of flips is 4 conditional on number of flips being even is " \
-    # EDIT: + repr(Solution2)) \
-    + "."
-
+print "The empirical probability that the number of flips is 4 conditional on number of flips being even is "
+print (repr(PrEven)  + ".")
 
 print "\nPart 2\n"
 
+def BiasedGemetricFlip(p = .5, q = .5);
+    control1 = 0
+    countA = 0
+    CountB = 0
+    Count = 0
+    while control1 ==0:
+        coinA1 = biasedcoinflip(p)
+        coinB1 = biasedcoinflip(q)
+    
+    if coinA1 == coinB1:
+        count +=1
+    elif (coinA1 + coinB1 ==1):
+        count +=1
+        countA +=1 coinA1
+        countB +=1 coinB1
+        control1 = 1
+return(count, countA, countB)
+    
 Trials2 = []
 FinalA = 0
 FinalB = 0
 for TrialIndex2 in range(0, NumberTrials):
-    #
-    # EDIT
-    #
+    GeometricResult = BiasedGeometricFlip(ParameterA, ParameterB)
+    Trials2.append(GeometricResult[0])
+    FinalC += GeometricResult[1]
+    FinalD += GeometricResult[2]
+    
+PrTwo = round(Trials2.count(2)/float(NumberTrials),3)
+CoinC = round(FinalC/float(NumberTrials),3)
+CoinD = round(FinalD/float(NumberTrials),3)
 
-print "The empirical probability that the number of flips is 2 is " \
-    # EDIT: + repr(Solution3)) \
-    + "."
-print "The empirical probability that coin A is showing 1 when the stopping condition is met is " \
-    # EDIT: + repr(Solution4)) \
-    + "."
-print "The empirical probability that coin B is showing 1 when the stopping condition is met is " \
-    # EDIT: + repr(Solution5)) \
-    + "."
+print ("The empirical probability that the number of flips is 2 is " + repr(PrTwo) + ".")
+print ("The empirical probability that coin C is showing 1 when the stopping condition is met is " + repr(CoinC) + ".")
+print ("The empirical probability that coin D is showing 1 when the stopping condition is met is " + repr(CoinD) + ".")
 
