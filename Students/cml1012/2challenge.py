ff44bca7eb269c8e01a98cc909dd891faa9a37f8
cml1012 <Cml1012@tamu.edu>
2015-10-05 20:33:25 -0500

    Update 2challenge.py

--- Students/cml1012/2challenge.py
+++ Students/cml1012/2challenge.py
@@ -1,10 +1,10 @@
-__author__ = ""
-__NetID__ = ""
-__GitHubID__ = ""
+__author__ = "Chu Liang"
+__NetID__ = "cml1012"
+__GitHubID__ = "cml1012"
 __challenge__ = "2"
 __version__ = "0.0"
 __grader__ = ""
-__SelfGrade__ = ""
+__SelfGrade__ = "4"
 __PeerGrade__ = ""
 
 """
@@ -22,50 +22,65 @@ ParameterP = 1.0/3.0    # Parameter of digital coin
 ParameterA = 1.0/3.0    # Parameter of digital coin A
 ParameterB = 1.0/2.0    # Parameter of digital coin B
 NumberTrials = 100000
-
+Trial1 = 4
+Trial2 = 2
 
 def biasedcoinflip(p=0.5):
-    """
-    This method returns a one with probability p and it returns a zero with
-    probability (1 - p). The default parameter is p=0.5; this can be changed
-    by passing an argument to the method.
-    """
+    for TrialIndex in range(0, NumberTrials):
+        TrialSequence.append(random.randrange(2))
+        if (sum(TrialSequence)/ (1.0 *len(TrialSequence))) < ParameterP:
+            TrialSequence[len(TrialSequence)-1] = 1
+            return 1
+        else:
+            TrialSequence[len(TrialSequence)-1] = 0
+            return 0
     return math.floor(random.random() + p)
 
 
 def geometricflip(p=0.5):
-    """
-    This method returns a natural number that denotes the number of digital
-    coin flips needed to obtain a one. It relies on method biasedcoinflip().
-    """
+    
     numberflips = 1
     while biasedcoinflip(p) != 1:
         numberflips += 1
     return numberflips
 
+def doubleflip(p1=0.5 , p2= 0.5):
+    numberflips = 1
+    count1 = 0
+    count2 = 0
+    
+    while (1):
+        coin1 = biasedcoinflip(p1)
+        coin2 = biasedcoinflip(p2)
+        if (coin1 == coin2):
+            numberflips += 1 
+        else:
+                break
+    if(coin1):
+        count1 += 1
+    elif(coin2):
+        count2 += 1
+    else:
+            break
+    return (numberflips, count1, count2)
 
 print "Part 1\n"
 
 Trials = []
 for TrialIndex1 in range(0, NumberTrials):
     Trials.append(geometricflip(ParameterP))
-#
-# EDIT
-#
-
-print "The empirical probability that the  number of flips is 4 is " \
-    # EDIT: + repr(Solution1)) \
-    + "."
-
+    Solution1 = round(Trials.count(Part1)/float(EvenTrials),3)
+    
 EvenTrials = 0
-for TrialIndex2 in range(0, NumberTrials):
-    #
-    # EDIT
-    #
+for TrialIndex1 in range(0, NumberTrials):
+    if(Trials[i]%2 == 0):
+         EvenTrials += 1
+    Solution2 = round(Trials.count(Part1)/float(EvenTrials), 3)
 
-print "The empirical probability that the number of flips is 4 conditional on number of flips being even is " \
-    # EDIT: + repr(Solution2)) \
-    + "."
+print "The empirical probability that the  number of flips is 4 is " 
+print(repr(Solution1))
+print "The empirical probability that the number of flips is 4 conditional on number of flips being even is " 
+print(repr(Solution2))
 
 
 print "\nPart 2\n"
@@ -74,17 +89,18 @@ Trials2 = []
 FinalA = 0
 FinalB = 0
 for TrialIndex2 in range(0, NumberTrials):
-    #
-    # EDIT
-    #
+    result_a = doubleflip(ParameterA, ParameterB)
+    Trial2.append(result_a[0])
+    FinalA += result_a[1]
+    FinalB += result_a[2]
 
-print "The empirical probability that the number of flips is 2 is " \
-    # EDIT: + repr(Solution3)) \
-    + "."
-print "The empirical probability that coin A is showing 1 when the stopping condition is met is " \
-    # EDIT: + repr(Solution4)) \
-    + "."
-print "The empirical probability that coin B is showing 1 when the stopping condition is met is " \
-    # EDIT: + repr(Solution5)) \
-    + "."
+Solution3 = round(Trials.count(Part1)/float(EvenTrials), 3)
+Solution4 = round(FinalA/float(NumberTrials),3)
+Solution5 = round(FinalB/float(NumberTrials),3)
 
+print "The empirical probability that the number of flips is 2 is " 
+print(repr(Solution3))    
+print "The empirical probability that coin A is showing 1 when the stopping condition is met is " 
+print(repr(Solution4))   
+print "The empirical probability that coin B is showing 1 when the stopping condition is met is " 
+print(repr(Solution5))    
