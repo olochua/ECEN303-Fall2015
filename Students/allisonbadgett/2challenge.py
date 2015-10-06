bd2fbdaecabad35940d0a50108abbeb5b46d1592
allisonbadgett <allisonbadgett95@gmail.com>
2015-10-06 00:06:15 -0500

    Update challenge 2

--- Students/allisonbadgett/2challenge.py
+++ Students/allisonbadgett/2challenge.py
@@ -1,10 +1,10 @@
-__author__ = ""
-__NetID__ = ""
-__GitHubID__ = ""
+__author__ = "Allison Badgett"
+__NetID__ = "allisonbadgett"
+__GitHubID__ = "allisonbadgett"
 __challenge__ = "2"
 __version__ = "0.0"
 __grader__ = ""
-__SelfGrade__ = ""
+__SelfGrade__ = "5"
 __PeerGrade__ = ""
 
 """
@@ -44,47 +44,50 @@ def geometricflip(p=0.5):
     return numberflips
 
 
-print "Part 1\n"
+print("Part 1\n") #altered to work with Python 3.4
 
+numFour =0
 Trials = []
 for TrialIndex1 in range(0, NumberTrials):
     Trials.append(geometricflip(ParameterP))
-#
-# EDIT
-#
+    if Trials[TrialIndex1] == 4:
+        numFour += 1    #increments if number of flips is 4
 
-print "The empirical probability that the  number of flips is 4 is " \
-    # EDIT: + repr(Solution1)) \
-    + "."
+print("The empirical probability that the  number of flips is 4 is " + repr(numFour/NumberTrials) + ".") #altered to work with Python 3.4
 
 EvenTrials = 0
 for TrialIndex2 in range(0, NumberTrials):
-    #
-    # EDIT
-    #
+    if Trials[TrialIndex2] % 2 == 0:
+        EvenTrials += 1 #increments if number of flips is even
 
-print "The empirical probability that the number of flips is 4 conditional on number of flips being even is " \
-    # EDIT: + repr(Solution2)) \
-    + "."
+print("The empirical probability that the number of flips is 4 conditional on number of flips being even is "+ repr(numFour/EvenTrials) + ".") #altered to work with Python 3.4
 
 
-print "\nPart 2\n"
+print("\nPart 2\n") #altered to work with Python 3.4
 
 Trials2 = []
 FinalA = 0
 FinalB = 0
+numTwo =0
+
 for TrialIndex2 in range(0, NumberTrials):
-    #
-    # EDIT
-    #
+    coinA = geometricflip(ParameterA)
+    coinB = geometricflip(ParameterB)
+    if coinA > coinB:   #if B is the stopping condition
+        Trials2.append(coinB)
+        FinalB += 1
+    elif coinA < coinB: #if A is the stopping condition
+        Trials2.append(coinA)
+        FinalA += 1
+    else:       #if both coins are stopping condition
+        Trials2.append(coinA)
+        FinalA += 1
+        FinalB += 1
 
-print "The empirical probability that the number of flips is 2 is " \
-    # EDIT: + repr(Solution3)) \
-    + "."
-print "The empirical probability that coin A is showing 1 when the stopping condition is met is " \
-    # EDIT: + repr(Solution4)) \
-    + "."
-print "The empirical probability that coin B is showing 1 when the stopping condition is met is " \
-    # EDIT: + repr(Solution5)) \
-    + "."
+for TrialIndex2 in range(0, NumberTrials):  #find number of times number of flips is 2
+    if Trials2[TrialIndex2] == 2:
+        numTwo += 1
 
+print("The empirical probability that the number of flips is 2 is "+ repr(numTwo/NumberTrials) + ".") #altered to work with Python 3.4
+print("The empirical probability that coin A is showing 1 when the stopping condition is met is "+ repr(FinalA/NumberTrials) +".") #altered to work with Python 3.4
+print("The empirical probability that coin B is showing 1 when the stopping condition is met is "+ repr(FinalB/NumberTrials)  + ".") #altered to work with Python 3.4
