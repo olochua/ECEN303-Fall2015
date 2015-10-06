d602ed467a3355c0dec256e6e354a2d73193afa7
Stephanie Demco <steph1995@tamu.edu>
2015-10-06 00:02:49 -0500

    Update 2challenge.py

--- Students/stephaniedemco/2challenge.py
+++ Students/stephaniedemco/2challenge.py
@@ -47,44 +47,65 @@ def geometricflip(p=0.5):
 print "Part 1\n"
 
 Trials = []
+probFour = 0
 for TrialIndex1 in range(0, NumberTrials):
     Trials.append(geometricflip(ParameterP))
-#
-# EDIT
-#
+    if Trials[TrialIndex1] == 4:
+        probFour += 1
 
-print "The empirical probability that the  number of flips is 4 is " \
-    # EDIT: + repr(Solution1)) \
-    + "."
+Solution1 = probFour/NumberTrials
+
+print "The empirical probability that the  number of flips is 4 is "+ repr(Solution1) + "."
 
 EvenTrials = 0
 for TrialIndex2 in range(0, NumberTrials):
-    #
-    # EDIT
-    #
+    if Trials[TrialIndex2]%2 == 0:
+        EvenTrials += 1.0
+Even = EvenTrials / NumberTrials
+Solution2 = Solution1 / Even
 
-print "The empirical probability that the number of flips is 4 conditional on number of flips being even is " \
-    # EDIT: + repr(Solution2)) \
-    + "."
+
+print "The empirical probability that the number of flips is 4 conditional on number of flips being even is "  + repr(Solution2)+ "."
 
 
 print "\nPart 2\n"
 
 Trials2 = []
-FinalA = 0
-FinalB = 0
+prob2 = 0
+probB = 0
+probA = 0
+
 for TrialIndex2 in range(0, NumberTrials):
-    #
-    # EDIT
-    #
+    FinalA = 0
+    FinalB = 0
+    Flips = 0
+    while FinalA == FinalB:
+        FinalA = biasedcoinflip(ParameterA)
+        FinalB = biasedcoinflip(ParameterB)
+        Flips += 1
+    Trials2.append(Flips)
+    if Trials2[TrialIndex2] == 2:
+        prob2 += 1
+    if FinalA == 1:
+        probA += 1
+    if FinalB == 1:
+        probB += 1
 
-print "The empirical probability that the number of flips is 2 is " \
-    # EDIT: + repr(Solution3)) \
-    + "."
+Solution3 = prob2/NumberTrials
+Solution4 = probA/NumberTrials
+Solution5 = probB/NumberTrials
+
+print "The empirical probability that the number of flips is 2 is " + repr(Solution3) + "."
+print "The empirical probability that coin A is showing 1 when the stopping condition is met is " + repr(Solution4)+ "."
+print "The empirical probability that coin B is showing 1 when the stopping condition is met is " + repr(Solution5) + "."
+
+
+
+
+print "The empirical probability that the number of flips is 2 is " + repr(Solution3) + "."
+
 print "The empirical probability that coin A is showing 1 when the stopping condition is met is " \
-    # EDIT: + repr(Solution4)) \
-    + "."
-print "The empirical probability that coin B is showing 1 when the stopping condition is met is " \
-    # EDIT: + repr(Solution5)) \
-    + "."
+      + repr(Solution4) + "."
 
+print "The empirical probability that coin B is showing 1 when the stopping condition is met is " \
+      + repr(Solution5) + "."
