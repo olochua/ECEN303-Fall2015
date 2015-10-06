6ebbc999e22188cc257e994daab5d9af717b899f
unknown <jpdominguez94@gmail.com>
2015-10-05 22:22:52 -0500

    Challenge 2

--- Students/jpdominguez94/2challenge.py
+++ Students/jpdominguez94/2challenge.py
@@ -4,7 +4,7 @@ __GitHubID__ = "jpdominguez94"
 __challenge__ = "2"
 __version__ = "1.0"
 __grader__ = ""
-__SelfGrade__ = ""
+__SelfGrade__ = "5"
 __PeerGrade__ = ""
 
 """
@@ -12,6 +12,8 @@ Random Signals and Systems
 Course: ECEN 303-502
 Maximum Grade: 5pt
 """
+
+#Using Pythin 3.x
 
 import random
 import math
@@ -49,42 +51,48 @@ print "Part 1\n"
 Trials = []
 for TrialIndex1 in range(0, NumberTrials):
     Trials.append(geometricflip(ParameterP))
-#
-# EDIT
-#
+  
+Solution1= 1.0*Trials.count(4) / len(Trials)    #times of event / total occurrences (added 1.0 to get the decimals)
 
-print "The empirical probability that the  number of flips is 4 is " \
-    # EDIT: + repr(Solution1)) \
-    + "."
+print("The empirical probability that the  number of flips is 4 is " + format(Solution1, '.5f' ))
 
-EvenTrials = 0
+EvenTrials = []
 for TrialIndex2 in range(0, NumberTrials):
-    #
-    # EDIT
-    #
+    EvenTrials_temp = []    #temp array to determine the even flips 
+    EvenTrials_temp.append(geometricflip(ParameterP)) #add all the trials to array
+    if EvenTrials_temp[0] % 2 == 0:     #if it's even add it to the main array
+        EvenTrials.append(EvenTrials_temp[0])
 
-print "The empirical probability that the number of flips is 4 conditional on number of flips being even is " \
-    # EDIT: + repr(Solution2)) \
-    + "."
+Solution2= 1.0*EvenTrials.count(4) / len(EvenTrials)    #times of event / total occurrences
 
+print("The empirical probability that the number of flips is 4 conditional on number of flips being even is "  
+    + format(Solution2, '.5f') + " ")
 
 print "\nPart 2\n"
 
 Trials2 = []
-FinalA = 0
-FinalB = 0
+TrialsA = []
+TrialsB = []
 for TrialIndex2 in range(0, NumberTrials):
-    #
-    # EDIT
-    #
+    FinalA = 0
+    FinalB = 0
+    counter = 0   #used to count the number of flips it takes
+            
+    while FinalA + FinalB != 1:  #keep flipping the coin until one of them is one and the other one isnt
+        FinalA = biasedcoinflip(ParameterA)
+        FinalB = biasedcoinflip(ParameterB)
+        counter += 1        
+    if FinalA == 1: #check to see if coin A is showing the 1
+        TrialsA.append(1)
+    else:            #check to see if coin B is showing the 1
+        TrialsB.append(1)
+    Trials2.append(counter)  #add the number of flips to the array
 
-print "The empirical probability that the number of flips is 2 is " \
-    # EDIT: + repr(Solution3)) \
-    + "."
-print "The empirical probability that coin A is showing 1 when the stopping condition is met is " \
-    # EDIT: + repr(Solution4)) \
-    + "."
-print "The empirical probability that coin B is showing 1 when the stopping condition is met is " \
-    # EDIT: + repr(Solution5)) \
-    + "."
+Solution3 = 1.0*Trials2.count(2) / len(Trials2) 
+Solution4 = 1.0*len(TrialsA) / len(Trials2)
+Solution5 = 1.0*len(TrialsB) / len(Trials2)
 
+
+print ("The empirical probability that the number of flips is 2 is " + format(Solution3, '.5f' ))
+print ("The empirical probability that coin A is showing 1 when the stopping condition is met is " + format(Solution4, '.5f' ))
+print ("The empirical probability that coin B is showing 1 when the stopping condition is met is " + format(Solution5, '.5f' ))
