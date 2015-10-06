eb3ea91fc49a72d9f2619e61f07f914381c382ab
smadi94 <smadi94@tamu.edu>
2015-10-05 22:49:08 -0500

    update peer grade

--- Students/alexgarcia2013/1challenge.py
+++ Students/alexgarcia2013/1challenge.py
@@ -3,9 +3,9 @@ __NetID__ = "alexgarcia2013"
 __GitHubID__ = "alexgarcia2013"
 __challenge__ = "1"
 __version__ = "0.0"
-__grader__ = ""
+__grader__ = "Zachary Smadi"
 __SelfGrade__ = "0"
-__PeerGrade__ = ""
+__PeerGrade__ = "0"
 
 """
 Random Signals and Systems
