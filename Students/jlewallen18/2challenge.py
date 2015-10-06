868cbe4f6c22e035bc10b860682cb515f62ea8dc
jlewallen18 <jlewallen18@gmail.com>
2015-10-05 22:07:52 -0500

    update 2challenge.py

--- Students/jlewallen18/2challenge.py
+++ Students/jlewallen18/2challenge.py
@@ -1,10 +1,10 @@
-__author__ = ""
-__NetID__ = ""
-__GitHubID__ = ""
+__author__ = "Jordan Lewallen"
+__NetID__ = "922009210"
+__GitHubID__ = "jlewallen18"
 __challenge__ = "2"
-__version__ = "0.0"
+__version__ = "1.0"
 __grader__ = ""
-__SelfGrade__ = ""
+__SelfGrade__ = "5"
 __PeerGrade__ = ""
 
 """
@@ -18,13 +18,15 @@ import math
 import matplotlib.pyplot as plt
 
 
-ParameterP = 1.0/3.0    # Parameter of digital coin
-ParameterA = 1.0/3.0    # Parameter of digital coin A
-ParameterB = 1.0/2.0    # Parameter of digital coin B
+ParameterP   = 1.0/3.0     # Parameter of digital coin
+ParameterA   = 1.0/3.0     # Parameter of digital coin A
+ParameterB   = 1.0/2.0    # Parameter of digital coin B
 NumberTrials = 100000
+first_trial      = 4
+second_trial     = 2
 
 
-def biasedcoinflip(p=0.5):
+def biasedcoinflip( p = 0.5 ):
     """
     This method returns a one with probability p and it returns a zero with
     probability (1 - p). The default parameter is p=0.5; this can be changed
@@ -33,7 +35,7 @@ def biasedcoinflip(p=0.5):
     return math.floor(random.random() + p)
 
 
-def geometricflip(p=0.5):
+def geometricflip( p = 0.5 ):
     """
     This method returns a natural number that denotes the number of digital
     coin flips needed to obtain a one. It relies on method biasedcoinflip().
@@ -43,48 +45,92 @@ def geometricflip(p=0.5):
         numberflips += 1
     return numberflips
 
+def doublegeometricflip( pr_A = 0.5 , pr_B = 0.5 ):
+	# this will calculate a double flip experiment
+	numberflips = 1
+	numberA = 0
+	numberB = 0
 
-print "Part 1\n"
+	while (1):
+		coin_A = biasedcoinflip(pr_A)
+		coin_B = biasedcoinflip(pr_B)
+		if (coin_A == coin_B):				# statement to flip the coins
+			numberflips += 1
+		else:
+			break
+	if (coin_A):
+		numberA += 1					# set when A is 1
+	elif(coin_B):
+		numberB += 1					# set when B is 1
+	else:
+		print ("\nThis is an ERROR\n")
+	return (numberflips, numberA, numberB)
+
+
+print "\nPart 1"
+print "(ParameterP = " \
+	  + repr(float(round(ParameterP,4))) \
+	  + ")"
 
 Trials = []
 for TrialIndex1 in range(0, NumberTrials):
     Trials.append(geometricflip(ParameterP))
-#
-# EDIT
-#
 
-print "The empirical probability that the  number of flips is 4 is " \
-    # EDIT: + repr(Solution1)) \
-    + "."
+Solution1 = round(Trials.count(first_trial)/float(NumberTrials), 4)
 
+# observe the even results
 EvenTrials = 0
+for i in range(0, NumberTrials):
+	if (Trials[i]%2 == 0):
+		EvenTrials += 1
+
+
+Solution2 = round(Trials.count(first_trial)/float(EvenTrials), 4)
+
+#print the first set of results here
+print "\nEmpirical probability that:"	\
+	+ "\n\tNumber of flips is " \
+	+ repr(first_trial) \
+	+ ": \t" \
+    + repr(Solution1) \
+	+ "\n\tNumber of flips is " \
+	+ repr(first_trial) \
+	+ ", given that number of flips is even: " \
+	+ repr(Solution2)
+
+
+#print second set of results here
+print "\nPart 2"
+print "(ParameterA = " \
+	  + repr(float(round(ParameterA,4))) \
+	  + ", ParameterB = " \
+	  + repr(float(round(ParameterB,4))) \
+	  + ")"
+
+Trials2 = []	
+FinalA = 0		
+FinalB = 0		
+
 for TrialIndex2 in range(0, NumberTrials):
-    #
-    # EDIT
-    #
+	results_i = doublegeometricflip(ParameterA, ParameterB)		
+	Trials2.append(results_i[0])
+	FinalA += results_i[1]
+	FinalB += results_i[2]
 
-print "The empirical probability that the number of flips is 4 conditional on number of flips being even is " \
-    # EDIT: + repr(Solution2)) \
-    + "."
+#below are the other set of tests!
+Solution3 = round(Trials2.count(second_trial)/float(NumberTrials), 3)
+Solution4 = round(FinalA/float(NumberTrials),4)
+Solution5 = round(FinalB/float(NumberTrials),4)
 
+# Print the answers
+print "\nEmpirical probability that:" \
+      + "\n\tNumber of trials is " \
+      + repr(second_trial) \
+      + ": \t" \
+      + repr(Solution3) \
+      + "\n\tCoin A is showing 1 when the stopping condition is met: " \
+      + repr(Solution4) \
+      + "\n\tCoin B is showing 1 when the stopping condition is met: " \
+      + repr(Solution5)
 
-print "\nPart 2\n"
-
-Trials2 = []
-FinalA = 0
-FinalB = 0
-for TrialIndex2 in range(0, NumberTrials):
-    #
-    # EDIT
-    #
-
-print "The empirical probability that the number of flips is 2 is " \
-    # EDIT: + repr(Solution3)) \
-    + "."
-print "The empirical probability that coin A is showing 1 when the stopping condition is met is " \
-    # EDIT: + repr(Solution4)) \
-    + "."
-print "The empirical probability that coin B is showing 1 when the stopping condition is met is " \
-    # EDIT: + repr(Solution5)) \
-    + "."
-
+print "\n"
