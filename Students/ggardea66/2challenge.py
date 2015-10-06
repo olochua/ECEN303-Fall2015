ff481227dbaa7eb2141fe3737329f3a7bb820ab3
Gissel Gardea <gardegi059@tamu.edu>
2015-10-05 21:32:34 -0500

    Update 2challenge.py

--- Students/ggardea66/2challenge.py
+++ Students/ggardea66/2challenge.py
@@ -1,10 +1,10 @@
-__author__ = ""
-__NetID__ = ""
-__GitHubID__ = ""
+__author__ = "Gissel Gardea"
+__NetID__ = "gardegi059"
+__GitHubID__ = "ggardea66"
 __challenge__ = "2"
-__version__ = "0.0"
+__version__ = "3.5.0"
 __grader__ = ""
-__SelfGrade__ = ""
+__SelfGrade__ = "4"
 __PeerGrade__ = ""
 
 """
@@ -21,10 +21,19 @@ import matplotlib.pyplot as plt
 ParameterP = 1.0/3.0    # Parameter of digital coin
 ParameterA = 1.0/3.0    # Parameter of digital coin A
 ParameterB = 1.0/2.0    # Parameter of digital coin B
-NumberTrials = 100000
-
+NumberTrials = 1000
+TrialSequence=[]
 
 def biasedcoinflip(p=0.5):
+    for TrialIndex in range(0,NumberTrials):
+        TrialSequence.append(random.randrange(2))
+        if (sum(TrialSequence)/(1.0*len(TrialSequence))) < ParameterP:
+            TrialSequence[len(TrialSequence)-1]=1
+            return 1
+        else:
+            TrialSequence[len(TrialSequence)-1] = 0
+            return 0
+
     """
     This method returns a one with probability p and it returns a zero with
     probability (1 - p). The default parameter is p=0.5; this can be changed
@@ -49,42 +58,54 @@ print "Part 1\n"
 Trials = []
 for TrialIndex1 in range(0, NumberTrials):
     Trials.append(geometricflip(ParameterP))
-#
-# EDIT
-#
 
-print "The empirical probability that the  number of flips is 4 is " \
-    # EDIT: + repr(Solution1)) \
-    + "."
+TrialFour = ((1.0*Trials.count(4))/(1.0*len(Trials)))
 
-EvenTrials = 0
+print ("The empirical probability that the  number of flips is 4 is {0:.4f}." .format(TrialFour))
+print(repr(Trials))
+
+ProbEven = []
 for TrialIndex2 in range(0, NumberTrials):
-    #
-    # EDIT
-    #
+    Evenfilter = []
+    Evenfilter.append(geometricflip(ParameterP))
+    if Evenfilter[0]%2 == 0:
+        ProbEven.append(Evenfilter[0])
+ProbIsEven= ((1.0*ProbEven.count(4)))/ (1.0*len(ProbEven))
 
-print "The empirical probability that the number of flips is 4 conditional on number of flips being even is " \
-    # EDIT: + repr(Solution2)) \
-    + "."
-
+print "The empirical probability that the number of flips is 4 conditional on number of flips being even is {0:.4f}." .format(ProbIsEven)
+print(repr(ProbEven))
 
 print "\nPart 2\n"
 
-Trials2 = []
-FinalA = 0
-FinalB = 0
-for TrialIndex2 in range(0, NumberTrials):
-    #
-    # EDIT
-    #
+CountOfFlips = []
+Heads = []
+for TrialIndex2 in range(0,NumberTrials):
+    FinalA = 0
+    FinalB = 0
+    Win = 0
+    count = 0
+    while Win == 0:
+        FinalA = biasedcoinflip(ParameterP)
+        FinalB = biasedcoinflip(ParameterP)
+        if (1.0*FinalA +1.0*FinalB) == 1.0:
+            Win = 1
+            count += 1
+            CountOfFlips.append(count)
+            Heads.append(FinalA)
+        else:
+            count += 1
+            Win = 0
 
-print "The empirical probability that the number of flips is 2 is " \
-    # EDIT: + repr(Solution3)) \
-    + "."
-print "The empirical probability that coin A is showing 1 when the stopping condition is met is " \
-    # EDIT: + repr(Solution4)) \
-    + "."
-print "The empirical probability that coin B is showing 1 when the stopping condition is met is " \
-    # EDIT: + repr(Solution5)) \
-    + "."
+TwoProb = ((1.0*CountOfFlips.count(2))/(1.0*len(CountOfFlips)))
+ProbA = ((1.0*Heads.count(1))/(1.0*len(Heads)))
+ProbB = ((1.0*Heads.count(0)))/ (1.0*len(Heads))
+
+print "The empirical probability that the number of flips is 2 is {0:.4f}." .format(TwoProb)
+print(repr(CountOfFlips))
+
+print "The empirical probability that coin A is showing 1 when the stopping condition is met is {0:.4f}." .format(ProbA)
+print(repr(Heads))
+print "The empirical probability that coin B is showing 1 when the stopping condition is met is {0:.4f}." .format(ProbB)
+print(repr(Heads))
+
 
