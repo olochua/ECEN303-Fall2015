392e8cfefbfb7c1eb7cfa050be12c0f667822014
smadi94 <smadi94@tamu.edu>
2015-10-06 23:27:22 -0500

    update  2challenge.py

--- Students/smadi94/2challenge.py
+++ Students/smadi94/2challenge.py
@@ -45,11 +45,12 @@ print "Part 1\n"
 
 Trials = []
 for TrialIndex1 in range(0, NumberTrials):
+
     Trials.append(geometricflip(ParameterP))
 PrFour = Trials.count(4)/float(NumberTrials)
 
-print ("The empirical probability that the  number of flips is 4 is " + repr(PrFour) + ".")
 
+print ("The empirical probability that the  number of flips is 4 is " + repr(PrFour) + ".")
 EvenTrials = 0
 for TrialIndex2 in range(0, NumberTrials):
     if Trials[TrialIndex2]%2 == 0:  # The Trials is multiple of 2
