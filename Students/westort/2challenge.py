d68ff7fb7520142a12a92e4487ce8fa2ad6be965
westort <wes4477@yahoo.com>
2015-10-06 20:59:27 -0500

    update challenge2.py

--- Students/westort/2challenge.py
+++ Students/westort/2challenge.py
@@ -1,10 +1,10 @@
-__author__ = ""
-__NetID__ = ""
-__GitHubID__ = ""
+__author__ = "Weston Torti"
+__NetID__ = "tort115"
+__GitHubID__ = "westort"
 __challenge__ = "2"
-__version__ = "0.0"
+__version__ = "1.0"
 __grader__ = ""
-__SelfGrade__ = ""
+__SelfGrade__ = "5"
 __PeerGrade__ = ""
 
 """
@@ -25,22 +25,19 @@ NumberTrials = 100000
 
 
 def biasedcoinflip(p=0.5):
-    """
-    This method returns a one with probability p and it returns a zero with
-    probability (1 - p). The default parameter is p=0.5; this can be changed
-    by passing an argument to the method.
-    """
+    if random.random() <= p: #standard coin flip biasing
+        return 1
+    else:
+        return 0
+
     return math.floor(random.random() + p)
 
 
 def geometricflip(p=0.5):
-    """
-    This method returns a natural number that denotes the number of digital
-    coin flips needed to obtain a one. It relies on method biasedcoinflip().
-    """
+
     numberflips = 1
     while biasedcoinflip(p) != 1:
-        numberflips += 1
+        numberflips += 1 #summing coin flips
     return numberflips
 
 
@@ -48,24 +45,19 @@ print "Part 1\n"
 
 Trials = []
 for TrialIndex1 in range(0, NumberTrials):
+
     Trials.append(geometricflip(ParameterP))
-#
-# EDIT
-#
+Solution1 = Trials.count(4)/float(NumberTrials)
 
-print "The empirical probability that the  number of flips is 4 is " \
-    # EDIT: + repr(Solution1)) \
-    + "."
 
+print ("The empirical probability that the  number of flips is 4 is %f." % Solution1)
 EvenTrials = 0
 for TrialIndex2 in range(0, NumberTrials):
-    #
-    # EDIT
-    #
-
-print "The empirical probability that the number of flips is 4 conditional on number of flips being even is " \
-    # EDIT: + repr(Solution2)) \
-    + "."
+    A= geometricflip(ParameterP)
+    if A % 2 == 0:
+       EvenTrials += 1
+Solution2 = Trials.count(4)/float(EvenTrials)
+print ("The empirical probability that the number of flips is 4 conditional on number of flips being even is %f " % Solution2)
 
 
 print "\nPart 2\n"
@@ -74,17 +66,25 @@ Trials2 = []
 FinalA = 0
 FinalB = 0
 for TrialIndex2 in range(0, NumberTrials):
-    #
-    # EDIT
-    #
+    numFlips = 0
+    CurrentA = 0
+    CurrentB = 0
+    while CurrentA == CurrentB:
+        CurrentA = biasedcoinflip(ParameterA)
+        CurrentB = biasedcoinflip(ParameterB)
+        numFlips += 1
+    Trials2.append(numFlips)
+    FinalA += CurrentA
+    FinalB += CurrentB
+    if numFlips == 2:
+        Trials2.append(1)
+Solution3 = Trials2.count(2)/float(NumberTrials)
+Solution4 = FinalA/float(NumberTrials)
+Solution5 = FinalB/float(NumberTrials)
 
-print "The empirical probability that the number of flips is 2 is " \
-    # EDIT: + repr(Solution3)) \
-    + "."
-print "The empirical probability that coin A is showing 1 when the stopping condition is met is " \
-    # EDIT: + repr(Solution4)) \
-    + "."
-print "The empirical probability that coin B is showing 1 when the stopping condition is met is " \
-    # EDIT: + repr(Solution5)) \
-    + "."
+print "The empirical probability that the number of flips is 2 is %f " % Solution3
+
+print "The empirical probability that coin A is showing 1 when the stopping condition is met is %f" % Solution4
+
+print "The empirical probability that coin B is showing 1 when the stopping condition is met is %f" % Solution5
 
