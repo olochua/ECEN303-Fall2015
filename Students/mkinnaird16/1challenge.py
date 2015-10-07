a74911a6db7df4938cb53715a6b8a94783cbc7de
alexgarcia2013 <alexgarcia2013@tamu.edu>
2015-10-06 21:22:32 -0500

    Update 1challenge.py

--- Students/mkinnaird16/1challenge.py
+++ Students/mkinnaird16/1challenge.py
@@ -3,9 +3,9 @@ __NetID__ = "mrk13"
 __GitHubID__ = "mkinnaird16"
 __challenge__ = "1"
 __version__ = "1.5"
-__grader__ = ""
+__grader__ = "Alexander Garcia"
 __SelfGrade__ = "4"
-__PeerGrade__ = ""
+__PeerGrade__ = "4"
 
 """
 Random Signals and Systems
