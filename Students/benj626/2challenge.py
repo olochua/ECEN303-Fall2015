c68eb4d39d892e7a1a5842502113b1e84258b98f
Ben Johnston <benj626@tamu.edu>
2015-10-06 21:17:37 -0500

    Update 2challenge.py

--- Students/benj626/2challenge.py
+++ Students/benj626/2challenge.py
@@ -4,7 +4,7 @@ __GitHubID__ = "benj626"
 __challenge__ = "2"
 __version__ = "0.0"
 __grader__ = ""
-__SelfGrade__ = ""
+__SelfGrade__ = "5"
 __PeerGrade__ = ""
 
 """
@@ -15,14 +15,16 @@ Maximum Grade: 5pt
 
 import random
 import math
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
+#Print statements were modified to work with Python 3.4
 
 ParameterP = 1.0/3.0    # Parameter of digital coin
 ParameterA = 1.0/3.0    # Parameter of digital coin A
 ParameterB = 1.0/2.0    # Parameter of digital coin B
 NumberTrials = 100000
-
+four_flips = 0
+two_flips = 0
 
 def biasedcoinflip(p=0.5):
     """
@@ -43,48 +45,61 @@ def geometricflip(p=0.5):
         numberflips += 1
     return numberflips
 
+print ("Part 1\n")
 
-print "Part 1\n"
 
 Trials = []
 for TrialIndex1 in range(0, NumberTrials):
     Trials.append(geometricflip(ParameterP))
-#
-# EDIT
-#
+    if Trials[TrialIndex1] == 4:
+        four_flips += 1
 
-print "The empirical probability that the  number of flips is 4 is " \
-    # EDIT: + repr(Solution1)) \
-    + "."
+print ("The empirical probability that the  number of flips is 4 is " \
+       + repr(four_flips/NumberTrials) \
+       + ".")
 
 EvenTrials = 0
 for TrialIndex2 in range(0, NumberTrials):
-    #
-    # EDIT
-    #
+    if Trials[TrialIndex2] % 2 == 0:
+        EvenTrials += 1
 
-print "The empirical probability that the number of flips is 4 conditional on number of flips being even is " \
-    # EDIT: + repr(Solution2)) \
-    + "."
+print ("The empirical probability that the number of flips is 4 conditional on number of flips being even is " \
+    + repr(four_flips/EvenTrials) \
+    + ".")
 
 
-print "\nPart 2\n"
+print ("\nPart 2\n")
 
 Trials2 = []
 FinalA = 0
 FinalB = 0
+
 for TrialIndex2 in range(0, NumberTrials):
-    #
-    # EDIT
-    #
+    coin_A = geometricflip(ParameterA)
+    coin_B = geometricflip(ParameterB)
+    if coin_A < coin_B:   #stops on A
+        Trials2.append(coin_A)
+        FinalA += 1
 
-print "The empirical probability that the number of flips is 2 is " \
-    # EDIT: + repr(Solution3)) \
-    + "."
-print "The empirical probability that coin A is showing 1 when the stopping condition is met is " \
-    # EDIT: + repr(Solution4)) \
-    + "."
-print "The empirical probability that coin B is showing 1 when the stopping condition is met is " \
-    # EDIT: + repr(Solution5)) \
-    + "."
+    elif coin_A > coin_B:     #stops on B
+        Trials2.append(coin_B)
+        FinalB += 1
 
+    else: #If they equal one another
+        Trials2.append(coin_A)
+        FinalA += 1
+        FinalB += 1
+
+for TrialIndex2 in range(0, NumberTrials):
+    if Trials2[TrialIndex2] == 2:
+        two_flips += 1
+
+print ("The empirical probability that the number of flips is 2 is " \
+    + repr(two_flips/NumberTrials) \
+    + ".")
+print ("The empirical probability that coin A is showing 1 when the stopping condition is met is " \
+    + repr(FinalA/NumberTrials) \
+    + ".")
+print ("The empirical probability that coin B is showing 1 when the stopping condition is met is " \
+    + repr(FinalB/NumberTrials) \
+    + ".")
