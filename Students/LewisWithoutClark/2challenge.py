7eea61d780f4c8cc1493771e1b8da0287af74954
Justin Lewis <justin94lewis@tamu.edu>
2015-10-07 07:39:06 -0500

    Submit Challenge 2
    oops, i meant to post this last night

--- Students/LewisWithoutClark/2challenge.py
+++ Students/LewisWithoutClark/2challenge.py
@@ -1,10 +1,10 @@
-__author__ = ""
-__NetID__ = ""
-__GitHubID__ = ""
+__author__ = "Justin Lewis"
+__NetID__ = "justin94lewis"
+__GitHubID__ = "LewisWithoutClark"
 __challenge__ = "2"
-__version__ = "0.0"
+__version__ = "1.0"
 __grader__ = ""
-__SelfGrade__ = ""
+__SelfGrade__ = "5.0"
 __PeerGrade__ = ""
 
 """
@@ -15,7 +15,6 @@ Maximum Grade: 5pt
 
 import random
 import math
-import matplotlib.pyplot as plt
 
 
 ParameterP = 1.0/3.0    # Parameter of digital coin
@@ -23,68 +22,91 @@ ParameterA = 1.0/3.0    # Parameter of digital coin A
 ParameterB = 1.0/2.0    # Parameter of digital coin B
 NumberTrials = 100000
 
-
 def biasedcoinflip(p=0.5):
-    """
-    This method returns a one with probability p and it returns a zero with
-    probability (1 - p). The default parameter is p=0.5; this can be changed
-    by passing an argument to the method.
-    """
-    return math.floor(random.random() + p)
+
+    return int( math.floor(random.random() + p))
 
 
 def geometricflip(p=0.5):
-    """
-    This method returns a natural number that denotes the number of digital
-    coin flips needed to obtain a one. It relies on method biasedcoinflip().
-    """
+
     numberflips = 1
     while biasedcoinflip(p) != 1:
         numberflips += 1
     return numberflips
 
+def geometricflip2(pA=0.5,pB=0.5):
+    a=0
+    b=0
+
+    numberflips = 0
+    while (a^b) != 1:
+        a = biasedcoinflip(pA)
+        b = biasedcoinflip(pB)
+        numberflips += 1
+    return numberflips
+
+def geometricflipCond(pA=0.5,pB=0.5,aVal=0,bVal=0):
+    a=0
+    b=0
+
+    while (a^b) != 1:
+        a = biasedcoinflip(pA)
+        b = biasedcoinflip(pB)
+
+    if (a == aVal) & (b == bVal):
+        return 1
+    else:
+        return 0
 
 print "Part 1\n"
 
 Trials = []
 for TrialIndex1 in range(0, NumberTrials):
     Trials.append(geometricflip(ParameterP))
-#
-# EDIT
-#
+
+Solution1 = Trials.count(4) / (1.0*NumberTrials)
 
 print "The empirical probability that the  number of flips is 4 is " \
-    # EDIT: + repr(Solution1)) \
-    + "."
+      + str(Solution1) + "."
 
 EvenTrials = 0
+
 for TrialIndex2 in range(0, NumberTrials):
-    #
-    # EDIT
-    #
+    if (Trials[TrialIndex2] % 2) == 0:
+        EvenTrials += 1
+
+Solution2 = (Solution1 * 1.0*NumberTrials) / (1.0*EvenTrials)
 
 print "The empirical probability that the number of flips is 4 conditional on number of flips being even is " \
-    # EDIT: + repr(Solution2)) \
-    + "."
+    + str(Solution2) + "."
 
 
 print "\nPart 2\n"
 
 Trials2 = []
-FinalA = 0
-FinalB = 0
-for TrialIndex2 in range(0, NumberTrials):
-    #
-    # EDIT
-    #
+
+for TrialIndex3 in range(0, NumberTrials):
+    Trials2.append(geometricflip2(ParameterA,ParameterB))
+
+Solution3 = Trials2.count(2) / (1.0*len(Trials2))
 
 print "The empirical probability that the number of flips is 2 is " \
-    # EDIT: + repr(Solution3)) \
-    + "."
-print "The empirical probability that coin A is showing 1 when the stopping condition is met is " \
-    # EDIT: + repr(Solution4)) \
-    + "."
-print "The empirical probability that coin B is showing 1 when the stopping condition is met is " \
-    # EDIT: + repr(Solution5)) \
-    + "."
+    + str(Solution3) + "."
 
+Trials3 = []
+for TrialIndex4 in range(0, NumberTrials):
+    Trials3.append(geometricflipCond(ParameterA,ParameterB,1,0))
+
+Solution4 = Trials3.count(1) / (1.0*len(Trials3))
+
+print "The empirical probability that coin A is showing 1 when the stopping condition is met is " \
+    + str(Solution4) + "."
+
+Trials4 = []
+for TrialIndex4 in range(0, NumberTrials):
+    Trials4.append(geometricflipCond(ParameterA,ParameterB,0,1))
+
+Solution5 = Trials4.count(1) / (1.0*len(Trials4))
+
+print "The empirical probability that coin B is showing 1 when the stopping condition is met is " \
+    + str(Solution5) + "."
