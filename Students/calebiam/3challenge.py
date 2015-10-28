d749d6d1d38ee70909bd488e5d3e4346e89dec58
Caleb Holley <calebiam@tamu.edu>
2015-10-27 21:05:27 -0500

    3challenge.py complete

--- Students/calebiam/3challenge.py
+++ Students/calebiam/3challenge.py
@@ -1,7 +1,7 @@
-__author__ = ""  # EDIT
-__NetID__ = ""  # EDIT
-__GitHubID__ = ""  # EDIT
-__SelfGrade__ = ""  # EDIT
+__author__ = "Caleb Holley "
+__NetID__ = "calebiam"
+__GitHubID__ = "calebiam"
+__SelfGrade__ = "5"
 __Challenge__ = "3"
 
 """
@@ -38,28 +38,38 @@ def binomialflips(n=1, p=0.5):
 
 
 def poisson(parameterpoisson=10):
-    #
-    # EDIT
-    #
+
+    probabilityrandom = random.random()
+    l = parameterpoisson
+
+    k = 0
+    sum = 0
+    while (1):
+        sum += pow(l,k)*math.exp(-1*l)/math.factorial(k)
+        if (sum > probabilityrandom):
+            break
+        k += 1
+    return k
 
 
 def experiment3(parameterpoisson3=10, p=0.5):
     return binomialflips(poisson(parameterpoisson3), p)
-    # return poisson(binomialflips(parameterpoisson3, p))
+    #return poisson(binomialflips(parameterpoisson3, p))
 
-
+#Experiment
 ParameterPoisson = 10
 NumberTrials = 100000
 TrialSequence = []
 
 for TrialIndex1 in range(0, NumberTrials):
     TrialSequence.append(experiment3(ParameterPoisson))
-print sum(TrialSequence)/len(TrialSequence)
+print(sum(TrialSequence)/len(TrialSequence))  #updated print statement for update python version
 
 Distribution = []
 for OutcomeIndex1 in range(0, 21):
     Distribution.append(TrialSequence.count(OutcomeIndex1) / (1.0 * NumberTrials))
 
+#Plotting
 OutcomeIndex2 = range(0, 21)
 num_bins = len(OutcomeIndex2)
 bar_width = 0.8
@@ -73,11 +83,12 @@ plt.xticks(XticksIndex, OutcomeIndex2)
 plt.show()
 
 # Question 1: What is the mean of experiment3()?
-# Answer 1: EDIT
+# Answer 1: The mean of experiment3() is 5.00107 or close to lambda/2.
 
 # Question 2: What is the type of experiment3()?
-# Answer 2: EDIT
+# Answer 2: experiment3() seems to resemble a Poisson Random Variable with lambda = 5 or lambda/2.
 
 # Question 3: Do the two distributions match?
-# Answer 3: EDIT
+# Answer 3: No they do not match, but they are very close to matching. The means of each distribution are very similar
+#              but are not exactly the same.
 
