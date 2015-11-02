b83ced1b559989b412cf677d9ba8359a878f08f1
thenderson37 <tyler.henderson07@tamu.edu>
2015-11-01 20:08:37 -0600

    Update 3challenge.py

--- Students/thenderson37/3challenge.py
+++ Students/thenderson37/3challenge.py
@@ -1,7 +1,7 @@
-__author__ = ""  # EDIT
-__NetID__ = ""  # EDIT
-__GitHubID__ = ""  # EDIT
-__SelfGrade__ = ""  # EDIT
+__author__ = "Tyler Henderson"
+__NetID__ = "tyler.henderson07"
+__GitHubID__ = "thenderson37"
+__SelfGrade__ = "5"
 __Challenge__ = "3"
 
 """
@@ -14,49 +14,72 @@ Maximum Grade: 5
 import random
 import math
 import matplotlib.pyplot as plt
+import numpy as np
 
+
+
+
+ParameterP = 1/3
+
+NumberTrials = 1000
+TrialSequence = []
 
 def biasedcoinflip(p=0.5):
-    """
-    This method returns a one with probability p and it returns a zero with
-    probability (1 - p). The default parameter is p=0.5; this can be changed
-    by passing an argument to the method.
-    """
+    for TrialIndex in range(0, NumberTrials):
+        TrialSequence.append(random.randrange(2))
+
+        if (sum(TrialSequence)/ (1.0 *len(TrialSequence))) < ParameterP:
+            TrialSequence[len(TrialSequence)-1] = 1
+            return 1
+
+        else:
+            TrialSequence[len(TrialSequence)-1] = 0
+            return 0
+
     return math.floor(random.random() + p)
 
 
+
 def binomialflips(n=1, p=0.5):
-    """
-    This method returns a binomial random variable with parameters n and p.
-    The default parameters are n=1 and p=0.5; this can be changed by passing
-    arguments to the method.
-    """
-    numberones = 0
+
+    bernouili_RV = np.random.binomial(n,p,NumberTrials)
+    return bernouili_RV[random.randint(0,len(bernouili_RV)-1)]
+
+    #This method returns a binomial random variable with parameters n and p.
+    #The default parameters are n=1 and p=0.5; this can be changed by passing
+    #arguments to the method.
+
+    Ones = 0
+
     for BinomialIndex in range(0,n):
-        numberones += biasedcoinflip(p)
-    return numberones
+        Ones += biasedcoinflip(p)
+    return Ones
+
 
 
 def poisson(parameterpoisson=10):
-    #
-    # EDIT
-    #
 
+    ps = np.random.poisson(parameterpoisson,NumberTrials)
+    return ps[random.randint(0,len(ps)-1)]
 
 def experiment3(parameterpoisson3=10, p=0.5):
     return binomialflips(poisson(parameterpoisson3), p)
-    # return poisson(binomialflips(parameterpoisson3, p))
 
+print("Poisson:")
+print(poisson())
 
 ParameterPoisson = 10
-NumberTrials = 100000
 TrialSequence = []
 
 for TrialIndex1 in range(0, NumberTrials):
     TrialSequence.append(experiment3(ParameterPoisson))
-print sum(TrialSequence)/len(TrialSequence)
+
+print("Mean:")
+print(sum(TrialSequence)/len(TrialSequence))
+
 
 Distribution = []
+
 for OutcomeIndex1 in range(0, 21):
     Distribution.append(TrialSequence.count(OutcomeIndex1) / (1.0 * NumberTrials))
 
@@ -67,17 +90,51 @@ XticksIndex = [(outcome + (0.5 * bar_width)) for outcome in OutcomeIndex2]
 opacity = 0.4
 
 plt.bar(OutcomeIndex2, Distribution, bar_width, alpha=opacity, color='b')
+plt.title("Experiment 3")
 plt.xlabel("Value")
 plt.ylabel("Probability")
 plt.xticks(XticksIndex, OutcomeIndex2)
 plt.show()
 
+def experiment4(parameterpoisson3=10, p=0.5):
+    return poisson(binomialflips(parameterpoisson3), p)
+
+print("Poisson:")
+print(poisson())
+
+ParameterPoisson = 10
+TrialSequence = []
+
+for TrialIndex1 in range(0, NumberTrials):
+    TrialSequence.append(experiment3(ParameterPoisson))
+
+print("Mean:")
+print(sum(TrialSequence)/len(TrialSequence))
+
+Distribution = []
+
+for OutcomeIndex1 in range(0, 21):
+    Distribution.append(TrialSequence.count(OutcomeIndex1) / (1.0 * NumberTrials))
+
+OutcomeIndex2 = range(0, 21)
+num_bins = len(OutcomeIndex2)
+bar_width = 0.8
+XticksIndex = [(outcome + (0.5 * bar_width)) for outcome in OutcomeIndex2]
+opacity = 0.4
+
+plt.bar(OutcomeIndex2, Distribution, bar_width, alpha=opacity, color='b')
+plt.title("Experiment 4")
+plt.xlabel("Value")
+plt.ylabel("Probability")
+plt.xticks(XticksIndex, OutcomeIndex2)
+plt.show()
+
+
 # Question 1: What is the mean of experiment3()?
-# Answer 1: EDIT
+# About 5
 
 # Question 2: What is the type of experiment3()?
-# Answer 2: EDIT
+# Poisson
 
 # Question 3: Do the two distributions match?
-# Answer 3: EDIT
-
+# They are similar but not exactly the same.
