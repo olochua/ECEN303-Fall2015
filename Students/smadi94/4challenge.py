d4d91b252615e272d7e89bea1e00e440e1eb885e
Jean-Francois Chamberland <chmbrlnd@tamu.edu>
2015-11-16 13:28:02 -0600

    Adding Challenge 4.

---
+++ Students/smadi94/4challenge.py
@@ -0,0 +1,57 @@
+__NetID__ = ""
+__GitHubID__ = ""
+__SelfGrade__ = ""
+__Challenge__ = "4"
+
+"""
+Random Signals and Systems
+Course: ECEN 303-502
+Maximum Grade: 5pt
+"""
+
+import random
+import math
+import numpy
+import pylab
+
+TrialNumber = 100000
+
+UniformList = []
+RayleighList = []
+for trial1 in range(0, TrialNumber):
+    UniformList.append(random.uniform(0, 2*math.pi))
+    RayleighList.append(numpy.random.rayleigh(1))
+
+pylab.figure()
+n, bins, patches = pylab.hist(UniformList, 1000, normed=1, histtype='stepfilled')
+pylab.setp(patches, 'facecolor', 'k', 'alpha', 0.75)
+
+pylab.figure()
+n, bins, patches = pylab.hist(RayleighList, 1000, normed=1, histtype='stepfilled')
+pylab.setp(patches, 'facecolor', 'g', 'alpha', 0.75)
+
+pylab.show()
+
+Sequence1 = []
+Sequence2 = []
+Sequence3 = []
+for trial2 in range(0, TrialNumber):
+    Sequence1.append(math.sin(UniformList[trial2]) * RayleighList[trial2])
+    Sequence2.append(math.cos(UniformList[trial2]) * RayleighList[trial2])
+    Sequence3.append(Sequence1[trial2]**2 + Sequence2[trial2]**2)
+
+#
+# EDIT
+#
+
+"""
+What is the type of random variable `Sequence1`?
+What is its mean and variance?
+What is the type of random variable `Sequence2`?
+What is its mean and variance?
+What is the type of random variable `Sequence3`?
+What is its mean and variance?
+What is the empirical covariance between `Sequence1` and `Sequence2`?
+Do you think they are independent? Justify your answer.
+"""
+
