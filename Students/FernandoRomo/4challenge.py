57e8b8d18e08a7c010502a249efb82c50cd4ad6a
J. Fernando Romo <jfernandoromoddl@outlook.com>
2015-11-23 23:00:34 -0600

    Update 4challenge.py

--- Students/FernandoRomo/4challenge.py
+++ Students/FernandoRomo/4challenge.py
@@ -1,6 +1,6 @@
-__NetID__ = ""
-__GitHubID__ = ""
-__SelfGrade__ = ""
+__NetID__ = "jfernandoromoddl"
+__GitHubID__ = "FernandoRomo"
+__SelfGrade__ = "5"
 __Challenge__ = "4"
 
 """
@@ -25,12 +25,13 @@ for trial1 in range(0, TrialNumber):
 pylab.figure()
 n, bins, patches = pylab.hist(UniformList, 1000, normed=1, histtype='stepfilled')
 pylab.setp(patches, 'facecolor', 'k', 'alpha', 0.75)
+pylab.title('UniformList')
 
 pylab.figure()
 n, bins, patches = pylab.hist(RayleighList, 1000, normed=1, histtype='stepfilled')
 pylab.setp(patches, 'facecolor', 'g', 'alpha', 0.75)
+pylab.title('RayleighList')
 
-pylab.show()
 
 Sequence1 = []
 Sequence2 = []
@@ -40,18 +41,59 @@ for trial2 in range(0, TrialNumber):
     Sequence2.append(math.cos(UniformList[trial2]) * RayleighList[trial2])
     Sequence3.append(Sequence1[trial2]**2 + Sequence2[trial2]**2)
 
-#
 # EDIT
-#
+#PLOT Sequence 1
+pylab.figure()
+n, bins, patches = pylab.hist(Sequence1, 1000, normed=1, histtype='stepfilled')
+pylab.setp(patches, 'facecolor', 'm', 'alpha', 0.75)
+pylab.title('Sequence1')
+print("Mean and Variance of Sequence 1")
+print(numpy.mean(Sequence1))
+print(numpy.var(Sequence1))
+
+#PLOT Sequence 2
+pylab.figure()
+n, bins, patches = pylab.hist(Sequence2, 1000, normed=1, histtype='stepfilled')
+pylab.setp(patches, 'facecolor', 'r', 'alpha', 0.75)
+pylab.title('Sequence2')
+print("Mean and Variance of Sequence 2")
+print(numpy.mean(Sequence2))
+print(numpy.var(Sequence2))
+
+#PLOT Sequence 3
+pylab.figure()
+n, bins, patches = pylab.hist(Sequence3, 1000, normed=1, histtype='stepfilled')
+pylab.setp(patches, 'facecolor', 'c', 'alpha', 0.75)
+pylab.title('Sequence3')
+print("Mean and Variance of Sequence 3")
+print(numpy.mean(Sequence3))
+print(numpy.var(Sequence3))
+
+#Variance comparison of Sequence 1 and Sequence 2
+print ("Covariance of Sequence 1 and Sequence 2")
+print (numpy.cov(Sequence1,Sequence2))
+
+pylab.show()
 
 """
 What is the type of random variable `Sequence1`?
+>> Continuous Random Variable with Rayleigh Distribution of a Sine.
 What is its mean and variance?
-What is the type of random variable `Sequence2`?
-What is its mean and variance?
-What is the type of random variable `Sequence3`?
-What is its mean and variance?
-What is the empirical covariance between `Sequence1` and `Sequence2`?
-Do you think they are independent? Justify your answer.
-"""
+>> Mean = 0, Variance = 1.
 
+What is the type of random variable `Sequence2`?
+>> Continuous Random Variable with Rayleigh Distribution of a Cosine.
+What is its mean and variance?
+>> Mean = 0, Variance = 1.
+
+What is the type of random variable `Sequence3`?
+>> Continuous Random Variable with a Rayleigh Distribution of a Cosine times a Sine.
+What is its mean and variance?
+>> Mean = 2, Variance = 4.
+
+What is the empirical covariance between `Sequence1` and `Sequence2`?
+>> The Empirical Covariance is close to zero.
+
+Do you think they are independent? Justify your answer.
+>> When two random variable distributions are independent, then the covariance will be zero therefore independent.
+"""
