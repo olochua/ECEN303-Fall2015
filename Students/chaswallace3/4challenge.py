904857ee7d4d7e9edac2e0986d94effa1516f964
Charles Wallace <c_wal43@yahoo.com>
2015-11-23 18:44:13 -0600

    Update challenge4.py

--- Students/chaswallace3/4challenge.py
+++ Students/chaswallace3/4challenge.py
@@ -1,6 +1,6 @@
-__NetID__ = ""
-__GitHubID__ = ""
-__SelfGrade__ = ""
+__NetID__ = "chaswallace3"
+__GitHubID__ = "chaswallace3"
+__SelfGrade__ = "5"
 __Challenge__ = "4"
 
 """
@@ -30,8 +30,6 @@ pylab.figure()
 n, bins, patches = pylab.hist(RayleighList, 1000, normed=1, histtype='stepfilled')
 pylab.setp(patches, 'facecolor', 'g', 'alpha', 0.75)
 
-pylab.show()
-
 Sequence1 = []
 Sequence2 = []
 Sequence3 = []
@@ -40,18 +38,66 @@ for trial2 in range(0, TrialNumber):
     Sequence2.append(math.cos(UniformList[trial2]) * RayleighList[trial2])
     Sequence3.append(Sequence1[trial2]**2 + Sequence2[trial2]**2)
 
-#
-# EDIT
-#
+pylab.figure()
+n, bins, patches = pylab.hist(Sequence1, 1000, normed=1, histtype='stepfilled')
+pylab.setp(patches, 'facecolor', 'r', 'alpha', 0.75)
+
+pylab.figure()
+n, bins, patches = pylab.hist(Sequence2, 1000, normed=1, histtype='stepfilled')
+pylab.setp(patches, 'facecolor', 'b', 'alpha', 0.75)
+
+pylab.figure()
+n, bins, patches = pylab.hist(Sequence3, 1000, normed=1, histtype='stepfilled')
+pylab.setp(patches, 'facecolor', 'k', 'alpha', 0.75)
+
+pylab.show()
+
+print("Mean of sequence 1:")
+print(sum(Sequence1)/len(Sequence1))
+print("Variance of sequence 1:")
+print(numpy.var(Sequence1))
+
+print("Mean of sequence 2:")
+print(sum(Sequence2)/len(Sequence2))
+print("Variance of sequence 2:")
+print(numpy.var(Sequence2))
+
+print("Mean of sequence 3:")
+print(sum(Sequence3)/len(Sequence3))
+print("Variance of sequence 3:")
+print(numpy.var(Sequence3))
+
+print("Covariance between Sequence 1 and Sequence 2:")
+print(numpy.cov([Sequence1,Sequence2]))
 
 """
 What is the type of random variable `Sequence1`?
+'Sequence1' appears to be a gaussian random variable
+
 What is its mean and variance?
+The mean of Sequence1 appears to be very close to 0, and its
+variance is close to 1
+
 What is the type of random variable `Sequence2`?
+'Sequence2' appears to be a gaussian random variable
+
 What is its mean and variance?
+The mean of Sequence2 appears to be very close to 0 and its
+variance is close to 1
+
 What is the type of random variable `Sequence3`?
+'Sequence3' appears to be an exponential random variable
+
 What is its mean and variance?
+The mean of Sequence3 appears to be close to 2 while the
+variance is close to 4
+
 What is the empirical covariance between `Sequence1` and `Sequence2`?
+The empirical covariance matrix appears to be the unit matrix. This indicates a
+covariance of 0.
+
 Do you think they are independent? Justify your answer.
+Yes, since the coariance matrix is very close to the identity matrix, it
+is safe to assume that Sequence1 and Sequence2 are independent.
 """
 
