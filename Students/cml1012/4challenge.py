86a357a0fd390fd43940a0e4fecd1eacf4384a92
cml1012 <Cml1012@tamu.edu>
2015-11-23 18:45:37 -0600

    Complete Challenge 4

--- Students/cml1012/4challenge.py
+++ Students/cml1012/4challenge.py
@@ -40,18 +40,66 @@ for trial2 in range(0, TrialNumber):
     Sequence2.append(math.cos(UniformList[trial2]) * RayleighList[trial2])
     Sequence3.append(Sequence1[trial2]**2 + Sequence2[trial2]**2)
 
-#
-# EDIT
-#
+pylab.figure()
+n, bins, patches = pylab.hist(Sequence1, 1000, normed = 1, histtype ='stepfilled')
+pylab.setup(patches, 'facecolor', 'y', 'alpha', .75)
+pylab.title('Sequence 1')
+
+pylab.figure()
+n, bins, patches = pylab.hist(Sequence2, 1000, normed = 1, histtype ='stepfilled')
+pylab.setup(patches, 'facecolor', 'r', 'alpha', .75)
+pylab.title('Sequence 2')
+
+pylab.figure()
+n, bins, patches = pylab.hist(Sequence3, 1000, normed = 1, histtype ='stepfilled')
+pylab.setup(patches, 'facecolor', 'c', 'alpha', .75)
+pylab.title('Sequence 3')
+
+pylab.show()
+
+print("Mean and Variance Sequence 1")
+print(numpy.mean(Sequence1))
+print(numpy.var(Sequence1))
+
+print(" ")
+
+print("Mean and Variance Sequence 2")
+print(numpy.mean(Sequence2))
+print(numpy.var(Sequence2))
+
+print(" ")
+
+print("Mean and Variance Sequence 3")
+print(numpy.mean(Sequence3))
+print(numpy.var(Sequence3))
+
+print(" ")
+
+print("Convariance of Sequence 1 & Sequence 2")
+print(numpy.covar(Sequence1, Sequence2))
 
 """
 What is the type of random variable `Sequence1`?
+    Continous Random Variable.
+
 What is its mean and variance?
+    The mean is about .00062, and the variance is about 1.0040.
+    
 What is the type of random variable `Sequence2`?
+    Continous Random Variable
+    
 What is its mean and variance?
+    The mean is .000005103, and the variance is .99708.
+    
 What is the type of random variable `Sequence3`?
+    Continous Random Variable
+    
 What is its mean and variance?
+    The mean is 2.0084, and the variance is 3.99945
+    
 What is the empirical covariance between `Sequence1` and `Sequence2`?
+    The empirical covariance between the 2 are close to 0.
 Do you think they are independent? Justify your answer.
+    Yes, because the covariance of sequence 1 and sequence 2 are about 0; therefore, making them independent of each other.
 """
 
