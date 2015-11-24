95abb619ca920251011848fd12573c4888ebcba6
nirm13ff05 <nirm13ff05@tamu.edu>
2015-11-23 19:54:56 -0600

    Update 4challenge.py

--- Students/nirm13ff05/4challenge.py
+++ Students/nirm13ff05/4challenge.py
@@ -25,12 +25,12 @@ for trial1 in range(0, TrialNumber):
 pylab.figure()
 n, bins, patches = pylab.hist(UniformList, 1000, normed=1, histtype='stepfilled')
 pylab.setp(patches, 'facecolor', 'k', 'alpha', 0.75)
+pylab.title('Uniform List')
 
 pylab.figure()
 n, bins, patches = pylab.hist(RayleighList, 1000, normed=1, histtype='stepfilled')
 pylab.setp(patches, 'facecolor', 'g', 'alpha', 0.75)
-
-pylab.show()
+pylab.title('Rayleigh List')
 
 Sequence1 = []
 Sequence2 = []
@@ -40,18 +40,62 @@ for trial2 in range(0, TrialNumber):
     Sequence2.append(math.cos(UniformList[trial2]) * RayleighList[trial2])
     Sequence3.append(Sequence1[trial2]**2 + Sequence2[trial2]**2)
 
-#
-# EDIT
-#
+#Sequence 1 Plot
+pylab.figure()
+n, bins, patches = pylab.hist(Sequence1, 1000, normed=1, histtype='stepfilled')
+pylab.setp(patches, 'facecolor', 'y', 'alpha', 0.75)
+pylab.title('Sequence 1')
 
+#Sequence 2 Plot
+pylab.figure()
+n, bins, patches = pylab.hist(Sequence2, 1000, normed=1, histtype='stepfilled')
+pylab.setp(patches, 'facecolor', 'r', 'alpha', 0.75)
+pylab.title('Sequence 2')
+
+#Sequence 3 Plot
+pylab.figure()
+n, bins, patches = pylab.hist(Sequence3, 1000, normed=1, histtype='stepfilled')
+pylab.setp(patches, 'facecolor', 'c', 'alpha', 0.75)
+pylab.title('Sequence 3')
+
+pylab.show()
+
+print("Mean and Variance of Sequence 1")  #edited print statements to work with updated version of python
+print(numpy.mean(Sequence1))
+print(numpy.var(Sequence1))
+
+print("  ")
+
+print("Mean and Variance of Sequence 2")
+print(numpy.mean(Sequence2))
+print(numpy.var(Sequence2))
+
+print("  ")
+
+print("Mean and Variance of Sequence 3")
+print(numpy.mean(Sequence3))
+print(numpy.var(Sequence3))
+
+print("  ")
+
+print ("Covariance of Sequence 1 and Sequence 2")
+print (numpy.cov(Sequence1,Sequence2))
 """
 What is the type of random variable `Sequence1`?
+    Continuous random variable. 
 What is its mean and variance?
+    Mean = -0.000623      Variance = 1.0041
 What is the type of random variable `Sequence2`?
+    Continuous random variable. 
 What is its mean and variance?
+    Mean = 5.1e-06      Variance = 0.997
 What is the type of random variable `Sequence3`?
+    Continuous random variable.
 What is its mean and variance?
+    Mean = 2.008        Variance = 3.999
 What is the empirical covariance between `Sequence1` and `Sequence2`?
+    Very close to 0. 
 Do you think they are independent? Justify your answer.
+    They must have a covariance equal to 0 to be independent, which they happen to have. Thuse they seem to independent. 
 """
 
