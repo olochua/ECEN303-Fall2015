dc45f48f476fc7a7cf4a2c0143e0a9a7499239d6
Kevin Bradshaw <kevintbradshaw@tamu.edu>
2015-11-24 00:02:24 -0600

    Completed 4challenge.py

--- Students/kevintbradshaw/4challenge.py
+++ Students/kevintbradshaw/4challenge.py
@@ -1,6 +1,6 @@
-__NetID__ = ""
-__GitHubID__ = ""
-__SelfGrade__ = ""
+__NetID__ = "kevintbradshaw"
+__GitHubID__ = "kevintbradshaw"
+__SelfGrade__ = "5"
 __Challenge__ = "4"
 
 """
@@ -40,18 +40,63 @@ for trial2 in range(0, TrialNumber):
     Sequence2.append(math.cos(UniformList[trial2]) * RayleighList[trial2])
     Sequence3.append(Sequence1[trial2]**2 + Sequence2[trial2]**2)
 
-#
-# EDIT
-#
+"EDIT:"
+"============================================================================================="
+pylab.figure()
+n, bins, patches = pylab.hist(Sequence1, 1000, normed=1, histtype='stepfilled')
+pylab.setp(patches, 'facecolor', 'y', 'alpha', 0.75)
+pylab.title('Sequence 1')
+print("Sequence 1: Mean")  
+print(numpy.mean(Sequence1))
+print("Sequence 1: Variance") 
+print(numpy.var(Sequence1))
+"============================================================================================="
+pylab.figure()
+n, bins, patches = pylab.hist(Sequence2, 1000, normed=1, histtype='stepfilled')
+pylab.setp(patches, 'facecolor', 'r', 'alpha', 0.75)
+pylab.title('Sequence 2')
+print("Sequence 2: Mean")  
+print(numpy.mean(Sequence2))
+print("Sequence 2: Variance")  
+print(numpy.var(Sequence2))
+"============================================================================================="
+pylab.figure()
+n, bins, patches = pylab.hist(Sequence3, 1000, normed=1, histtype='stepfilled')
+pylab.setp(patches, 'facecolor', 'c', 'alpha', 0.75)
+pylab.title('Sequence 3')
+print("Sequence 3: Mean")  
+print(numpy.mean(Sequence3))
+print("Sequence 3: Variance")  
+print(numpy.var(Sequence3))
+"============================================================================================="
+print ("Covariance of Sequence 1 and Sequence 2")
+print (numpy.cov(Sequence1,Sequence2))
+"============================================================================================="
+pylab.show()
 
 """
 What is the type of random variable `Sequence1`?
+It's a continuous random variable with a rayleigh distribution dependent on a sinusoidal function.
+
 What is its mean and variance?
+The mean is 0 and the variance is 1.
+
 What is the type of random variable `Sequence2`?
+It's a continuous random variable with a rayleigh distribution dependent on a cosinusoidal function.
+
 What is its mean and variance?
+The mean is 0 and the variance is 1.
+
 What is the type of random variable `Sequence3`?
+It's a continuous random variable with a rayleigh distribution dependent on a multiplied sinusoidal and cosinusoidal function.
+
 What is its mean and variance?
+The mean is 2 and the variance is 4.
+
 What is the empirical covariance between `Sequence1` and `Sequence2`?
+The emperical covariance is 0.
+
 Do you think they are independent? Justify your answer.
+Yes because the covariance is zero and this is only true when two random variables distributions are independent. 
 """
 
