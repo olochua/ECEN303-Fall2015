1bb328ee524fbfbbdea15bda063608f4e29c8f3e
Justin Lewis <justin94lewis@tamu.edu>
2015-11-24 19:54:02 -0600

    Update 4challenge.py

--- Students/LewisWithoutClark/4challenge.py
+++ Students/LewisWithoutClark/4challenge.py
@@ -1,6 +1,6 @@
-__NetID__ = ""
-__GitHubID__ = ""
-__SelfGrade__ = ""
+__NetID__ = "justin94lewis"
+__GitHubID__ = "LewisWithoutClark"
+__SelfGrade__ = "5"
 __Challenge__ = "4"
 
 """
@@ -30,8 +30,6 @@ pylab.figure()
 n, bins, patches = pylab.hist(RayleighList, 1000, normed=1, histtype='stepfilled')
 pylab.setp(patches, 'facecolor', 'g', 'alpha', 0.75)
 
-pylab.show()
-
 Sequence1 = []
 Sequence2 = []
 Sequence3 = []
@@ -40,18 +38,77 @@ for trial2 in range(0, TrialNumber):
     Sequence2.append(math.cos(UniformList[trial2]) * RayleighList[trial2])
     Sequence3.append(Sequence1[trial2]**2 + Sequence2[trial2]**2)
 
-#
-# EDIT
-#
+pylab.figure()
+n, bins, patches = pylab.hist(Sequence1, 1000, normed=1, histtype='stepfilled')
+pylab.setp(patches, 'facecolor', 'k', 'alpha', 0.75)
+
+pylab.figure()
+n, bins, patches = pylab.hist(Sequence2, 1000, normed=1, histtype='stepfilled')
+pylab.setp(patches, 'facecolor', 'g', 'alpha', 0.75)
+
+pylab.figure()
+n, bins, patches = pylab.hist(Sequence3, 1000, normed=1, histtype='stepfilled')
+pylab.setp(patches, 'facecolor', 'g', 'alpha', 0.75)
+
+E1 = numpy.mean(Sequence1);
+E2 = numpy.mean(Sequence2);
+E3 = numpy.mean(Sequence3);
+
+V1 = numpy.var(Sequence1);
+V2 = numpy.var(Sequence2);
+V3 = numpy.var(Sequence3);
+
+
+print(str(E1));
+print(str(E2));
+print(str(E3));
+print(str(V1));
+print(str(V2));
+print(str(V3));
+
+
+print(numpy.cov(Sequence1,Sequence2))
+
+pylab.show;
+
 
 """
 What is the type of random variable `Sequence1`?
-What is its mean and variance?
-What is the type of random variable `Sequence2`?
-What is its mean and variance?
-What is the type of random variable `Sequence3`?
-What is its mean and variance?
-What is the empirical covariance between `Sequence1` and `Sequence2`?
-Do you think they are independent? Justify your answer.
-"""
 
+Gaussian Random Variable
+
+What is its mean and variance?
+
+mean: 0    variance: 1
+
+What is the type of random variable `Sequence2`?
+
+Gaussian Random Variable
+
+What is its mean and variance?
+
+mean: 0    variance: 1
+
+What is the type of random variable `Sequence3`?
+
+Exponential Random Variable
+
+What is its mean and variance?
+
+mean: 2    variance: 4
+
+What is the empirical covariance between `Sequence1` and `Sequence2`?
+
+covariance: 0  (6.46376479e-04 for one run, which is roughly zero)
+
+Do you think they are independent? Justify your answer.
+
+by analysis of the covariance, the sequences are likely independent. If two 
+sequences are independent, then their covariance is necessarily zero; however,
+if the covariance of two random variables is zero, that does NOT necessitate 
+that they are independent. They are only independent if they are jointly 
+normally distributed. We haven't gone over this yet so I'm guessing they are
+actually independent, but one way to know for sure would be to look at
+thier joint distribution. 
+
+"""
