3f67a9c6435c29effffd876be7d79e9336d833a6
lgnbarnard <lgn_barnard@tamu.edu>
2015-11-24 23:02:54 -0600

    Finished Challenge 4

--- Students/lgnbarnard/4challenge.py
+++ Students/lgnbarnard/4challenge.py
@@ -1,6 +1,6 @@
-__NetID__ = ""
-__GitHubID__ = ""
-__SelfGrade__ = ""
+__NetID__ = "lgn_barnard"
+__GitHubID__ = "lgnbarnard"
+__SelfGrade__ = "5"
 __Challenge__ = "4"
 
 """
@@ -30,7 +30,6 @@ pylab.figure()
 n, bins, patches = pylab.hist(RayleighList, 1000, normed=1, histtype='stepfilled')
 pylab.setp(patches, 'facecolor', 'g', 'alpha', 0.75)
 
-pylab.show()
 
 Sequence1 = []
 Sequence2 = []
@@ -40,18 +39,78 @@ for trial2 in range(0, TrialNumber):
     Sequence2.append(math.cos(UniformList[trial2]) * RayleighList[trial2])
     Sequence3.append(Sequence1[trial2]**2 + Sequence2[trial2]**2)
 
-#
-# EDIT
-#
+
+
+#Sequence 1
+pylab.figure()
+n, bins, patches = pylab.hist(Sequence1, 1000, normed=1, histtype='stepfilled')
+pylab.setp(patches, 'facecolor', 'm', 'alpha', 0.75)
+pylab.title('Sequence 1')
+
+#S1 Mean and Var
+E1 = numpy.mean(Sequence1);
+V1 = numpy.var(Sequence1);
+
+
+
+#Sequence 2
+pylab.figure()
+n, bins, patches = pylab.hist(Sequence2, 1000, normed=1, histtype='stepfilled')
+pylab.setp(patches, 'facecolor', 'r', 'alpha', 0.75)
+pylab.title('Sequence 2')
+
+#S2 Mean and Var
+E2 = numpy.mean(Sequence2);
+V2 = numpy.var(Sequence2);
+
+
+
+#Sequence 3
+pylab.figure()
+n, bins, patches = pylab.hist(Sequence3, 1000, normed=1, histtype='stepfilled')
+pylab.setp(patches, 'facecolor', 'c', 'alpha', 0.75)
+pylab.title('Sequence 3')
+
+#S3 Mean and Var
+E3 = numpy.mean(Sequence3);
+V3 = numpy.var(Sequence3);
+
+
+
+#Print
+print(str(E1));
+print(str(V1));
+print(str(E2));
+print(str(V2));
+print(str(E3));
+print(str(V3));
+
+print(numpy.cov(Sequence1,Sequence2))
+
+pylab.show()
 
 """
 What is the type of random variable `Sequence1`?
-What is its mean and variance?
-What is the type of random variable `Sequence2`?
-What is its mean and variance?
-What is the type of random variable `Sequence3`?
-What is its mean and variance?
-What is the empirical covariance between `Sequence1` and `Sequence2`?
-Do you think they are independent? Justify your answer.
-"""
+Gaussian Random Variable
 
+What is its mean and variance?
+mean is about 0 and variance is about 1
+
+What is the type of random variable `Sequence2`?
+Gaussian Random Variable
+
+What is its mean and variance?
+mean is about 0 and variance is about 1
+
+What is the type of random variable `Sequence3`?
+Exponential Random Variable
+
+What is its mean and variance?
+mean is about 2 variance is about 4
+
+What is the empirical covariance between `Sequence1` and `Sequence2`?
+close to 0
+
+Do you think they are independent? Justify your answer.
+With the covariance close to 0 I am able to infer that they are independent
+"""
